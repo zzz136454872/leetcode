74750374382a5b50e953264ca9faa8667bc0d6be
f4prime <zzz136454872@163.com>
2020-03-10 16:11:36 +0800

    fix a bug

--- diameterOfBinaryTree.py
+++ diameterOfBinaryTree.py
@@ -1,35 +1,29 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        return get_dis(root,0)
+        if root==None:
+            return 0
+        self.log(root)
+        return self.get_dis(root,0)
 
-    def log(root):
+    def log(self,root):
         if root.left!=None:
-            log(root.left)
+            self.log(root.left)
         if root.right!=None:
-            log(root.right)
+            self.log(root.right)
         if root.left==None:
             root.left_end=0
         else:
-            root.left=max(root.left.left_end,root.left.right_end)+1
-        if root.right_end==None:
-            right=0
+            root.left_end=max(root.left.left_end,root.left.right_end)+1
+        if root.right==None:
+            root.right_end=0
         else:
-            root.right=max(root.right.left_end,root.right.right_end)+1
+            root.right_end=max(root.right.left_end,root.right.right_end)+1
 
-    def get_dis(root,max_dis):
+    def get_dis(self,root,max_dis):
         if root.left!=None:
-            max_dis=max(max_dis,get_dis(root.left,max_dis))
+            max_dis=max(max_dis,self.get_dis(root.left,max_dis))
         if root.right!=None:
-            max_dis=max(max_dis,get_dis(root.right,max_dis))
+            max_dis=max(max_dis,self.get_dis(root.right,max_dis))
         max_dis=max(max_dis,root.left_end+root.right_end)
         return max_dis
-
-
-
 
