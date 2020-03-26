5501670a60f6a82208ddf5102a366305ef4968b1
f4prime <zzz136454872@163.com>
2020-03-26 17:46:31 +0800

    add one

---
+++ numRookCaptures.py
@@ -0,0 +1,53 @@
+from typing import *
+
+class Solution:
+    def numRookCaptures(self, board: List[List[str]]) -> int:
+        x=0
+        y=0
+        for i in range(8):
+            for j in range(8):
+                if board[i][j]=='R':
+                    x=j
+                    y=i
+                    break
+            if x > 0 or y>0:
+                break
+        capture=0
+        i=y
+        j=x
+        #up
+        while j>=0:
+            if board[i][j]=='B':
+                break
+            if board[i][j]=='p':
+                capture+=1
+            j-=1
+        j=x
+        #left
+        while i>=0:
+            if board[i][j]=='B':
+                break
+            if board[i][j]=='p':
+                capture+=1
+            i-=1
+        #down
+        i=y
+        while j<8:
+            if board[i][j]=='B':
+                break
+            if board[i][j]=='p':
+                capture+=1
+            j+=1
+        #right
+        j=x
+        while i<8:
+            if board[i][j]=='B':
+                break
+            if board[i][j]=='p':
+                capture+=1
+            i+=1
+        return capture
+    
+sl=Solution()
+inp=[[".",".",".",".",".",".",".","."],[".","p","p","p","p","p",".","."],[".","p","p","B","p","p",".","."],[".","p","B","R","B","p",".","."],[".","p","p","B","p","p",".","."],[".","p","p","p","p","p",".","."],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]]
+print(sl.numRookCaptures(inp))
