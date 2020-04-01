0a913e89d23f18ca452251cb7ae83b08dfa511d5
f4prime <zzz136454872@163.com>
2020-04-01 11:03:38 +0800

    add one

---
+++ maxDepthAfterSplit.cpp
@@ -0,0 +1,110 @@
+/**
+ * @author f4prime
+ * @email zzz136454872@163.com
+ * @aim a plain model for leetcode
+ */
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+//显示中间结果
+void print(vector<int> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+
+void print(vector<vector<int>> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+    {
+        for(int j=0;j<(int)a[i].size();j++)
+            cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+}
+
+void print(vector<bool> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+    {
+        cout<<i<<" ";
+        if(a[i])
+            cout<<"true ";
+        else
+            cout<<"false ";
+    }
+    cout<<endl;
+}
+
+void print(bool a[],int len)
+{
+    for(int i=0;i<len;i++)
+    {
+        cout<<i<<" ";
+        if(a[i])
+            cout<<"true ";
+        else
+            cout<<"false ";
+    }
+    cout<<endl;
+}
+
+//#define testMod
+
+#ifdef testMod
+void test()
+{
+    
+}
+#endif
+
+#ifndef testMod
+class Solution {
+public:
+    public:
+    vector<int> maxDepthAfterSplit(string seq) {
+        vector<int> out;
+        
+        for(int i=0;i<(int)seq.size();i++)
+        {
+            if(i%2==0)
+                out.push_back(seq[i]=='('?1:0);
+            else
+                out.push_back(seq[i]==')'?1:0);
+        }
+            return out;
+    }
+
+
+        
+private:
+};
+
+
+
+#endif
+
+int main()
+{
+#ifdef testMod
+    test();
+#endif
+
+#ifndef testMod
+    Solution sl;
+
+
+    
+#endif 
+    return 0;
+}
+
