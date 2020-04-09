4e6dacb2e0f4ee8ca44c3e5f07e7fde6926405e5
f4prime <zzz136454872@163.com>
2020-04-09 11:57:05 +0800

    add one

---
+++ isOneBitCharacter.py
@@ -0,0 +1,16 @@
+from typing import *
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        last=''
+        while len(bits)>0:
+            if bits[0]==1:
+                last=bits[:2]
+                bits=bits[2:]
+            else:
+                last=bits[:1]
+                bits=bits[1:]
+        return last[0]==0
+                
+                
+            
