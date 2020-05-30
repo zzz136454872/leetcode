253e51bc4a08bef08017eba34ead81628853b8ee
f4prime <zzz136454872@163.com>
2020-05-30 11:38:07 +0800

    add one

---
+++ largestRectangleArea.py
@@ -0,0 +1,25 @@
+from typing import *
+
+class Solution:
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
+        heights.insert(0,0)
+        stack=[]
+        out=0
+        print(heights)
+        for i in range(len(heights)):
+            while len(stack)>0 and heights[stack[-1]]>heights[i]:
+                print(i,stack,out)
+                mid=stack.pop()
+                start=stack[-1]+1
+                end=i-1
+                width=end-start+1
+                out=max(out,width*heights[mid])
+            stack.append(i)
+        return out
+
+inp=[2,1,5,6,2,3]
+sl=Solution()
+print(sl.largestRectangleArea(inp))
+            
