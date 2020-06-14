72150ab51a238c5af2c3abb0ea6084ed5a8c0f40
f4prime <zzz136454872@163.com>
2020-06-14 18:02:01 +0800

    add one

--- majorityElement.py
+++ majorityElement.py
@@ -1,4 +1,7 @@
-class Solution:
+from typing import *
+
+# 求众数1
+class Solution1:
     def majorityElement(self, nums) :
         log=dict()
         for num in nums:
@@ -10,6 +13,41 @@ class Solution:
             if log[key]>=len(nums)/2:
                 return key
         return 0
-                
+
+#求众数2
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]: 
+        major=[]
+        count=[]
+        for num in nums:
+            find=False
+            for i in range(len(major)):
+                if num==major[i]:
+                    count[i]+=1
+                    find=True
+            if find:
+                continue
+            if len(major)<2:
+                count.append(1)
+                major.append(num)
+                continue
+            count[1]-=1
+            if count[1]==0:
+                del count[1]
+                del major[1]
+            count[0]-=1
+            if count[0]==0:
+                del count[0]
+                del major[0]
+        out=[]
+        for num in major:
+            if nums.count(num)>len(nums)//3:
+                out.append(num)
+        return out
+
+sl=Solution()
+
+inp=[1,1,1,3,3,2,2,2]
+print(sl.majorityElement(inp))
 
         
