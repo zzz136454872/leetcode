ac40d2e1d5dfb6baef33fa7182bab9211ba66298
f4prime <zzz136454872@163.com>
2020-06-15 09:23:23 +0800

    add one

---
+++ MapSum.py
@@ -0,0 +1,49 @@
+from typing import *
+
+class Node:
+    def __init__(self,value):
+        self.son=[None for i in range(26)]
+        self.value=value
+    
+    def print(self):
+        print(self.value,':',end=' ')
+        for i in range(len(self.son)):
+            print(chr(ord('a')+i),self.son[i],end='')
+
+class MapSum:
+    def __init__(self):
+        self.root=Node(0)
+
+    def insert(self, key: str, val: int) -> None:
+        root=self.root
+        for letter in key:
+            num=ord(letter)-ord('a')
+            if root.son[num]==None:
+                root.son[num]=Node(0)
+            root=root.son[num]
+        root.value=val
+        #print(root.value)
+
+    def sum(self, prefix: str) -> int:
+        root=self.root
+        for letter in prefix:
+            num=ord(letter)-ord('a')
+            if root.son[num]==None:
+                print('false')
+                return 0
+            root=root.son[num]
+        return self.son_sum(root)
+
+    def son_sum(self,root):
+        if root==None:
+            return 0
+        out=root.value
+        for i in range(26):
+            out+=self.son_sum(root.son[i])
+        return out
+
+obj = MapSum()
+obj.insert("apple", 3)
+print(obj.sum("ap"))
+obj.insert("app", 2)
+print(obj.sum("ap"))
---
+++ makeArrayIncreasing.py
@@ -0,0 +1,26 @@
+from typing import *
+
+class Solution:
+    def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
+        arr2=list(set(arr2)).sort()
+        length=max(len(arr1),len(arr2))
+        dp=[0 for i in range(len(arr1))] for j in range(length)]
+
+        for i in range(len(arr1)):
+        
+        for i in range(length):
+            for j in range(len(arr1)):
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
