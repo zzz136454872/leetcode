bb5f07b8c9956aad3ca4704495941fa2ec9bcfaf
f4prime <zzz136454872@163.com>
2020-06-27 16:36:23 +0800

    add one

--- firstMissingPositive.py
+++ firstMissingPositive.py
@@ -13,17 +13,37 @@ class Solution:
         #        return get+1
         #    get=tmp[i]
         #return get+1
-        nums.sort()
-        get=0
-        for i in range(len(nums)):
-            if(nums[i] <= 0):
-                continue
-            if nums[i]>get+1:
-                return get+1
-            get=nums[i]
-        return get+1
+        # nums.sort()
+        # get=0
+        # for i in range(len(nums)):
+        #     if(nums[i] <= 0):
+        #         continue
+        #     if nums[i]>get+1:
+        #         return get+1
+        #     get=nums[i]
+        # return get+1
+        
+        n=len(nums)
+        if n==0:
+            return 1
+        flag=(nums[0]==n)
+        i=0
+        while i<n:
+            if nums[i]!=i and nums[i]<n and nums[i]>=0 and nums[i]!=nums[nums[i]]:
+                tmp=nums[i]
+                nums[i]=nums[nums[i]]
+                nums[tmp]=tmp
+            else:
+                i+=1
+        for i in range(1,n):
+            if i != nums[i]:
+                return i
+        if flag or nums[0]==n:
+            return n+1
+        else:
+            return n
 
-nums = [-1,2,3]
+nums = [1,2,3]#,-1,1]
 sl=Solution()
 print(sl.firstMissingPositive(nums))
 
