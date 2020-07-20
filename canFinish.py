ca4543fb42484dfe938c15aad0d1fb6b31b615c0
f4prime <zzz136454872@163.com>
2020-07-20 21:05:44 +0800

    add one

---
+++ canFinish.py
@@ -0,0 +1,42 @@
+from typing import *
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        log=[[] for i in range(numCourses)]
+        log2=[[] for i in range(numCourses)]
+        visited=[False for i in range(numCourses)]
+        for depend in prerequisites:
+            log[depend[0]].append(depend[1])
+            log2[depend[1]].append(depend[0])
+        print(log)
+        print(log2)
+        def visit(a):
+            visited[a]=True
+            for course in log2[a]:
+                log[course].remove(a)
+                if not visited[course] and len(log[course])==0:
+                    visit(course)
+        i=0
+        while i<numCourses:
+            if visited[i]:
+                i+=1
+                continue
+            if log[i]==[]:
+                visit(i)
+                i=0
+            else:
+                i+=1
+            print('flag')
+        print(visited)
+        return not (False in visited)
+inp1=3
+inp2=[[1,0],[2,1],[0,2]] 
+sl=Solution()
+print(sl.canFinish(inp1,inp2))
+
+
+
+                    
+            
+
+    
