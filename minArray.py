5283890efd45aa78cfefc63f0b0a556961cb4875
f4prime <zzz136454872@163.com>
2020-07-22 09:29:16 +0800

    update one

--- minArray.py
+++ minArray.py
@@ -7,16 +7,16 @@ class Solution:
         end=len(nums)-1
         if nums[start] < nums[end]:
             return nums[0]
-        while start<end-1:
+        while start<end:
             mid=(start+end)//2
-            print(start,mid,end)
-            if nums[mid]<nums[end]:
+            #print(start,mid,end)
+            if nums[mid]>nums[end]:
+                start=mid+1
+            elif nums[mid]<nums[end]:
                 end=mid
-            elif nums[mid]>nums[end]:
-                start=mid
             else:
-                start+=1
-        return nums[end]
+                end-=1
+        return nums[start]
 
 sl=Solution()
 inp=[2,1,2,2,2]
