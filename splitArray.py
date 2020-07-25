bb804baf4a487978bf863918e49f7b7cd05741f6
f4prime <zzz136454872@163.com>
2020-07-25 10:43:58 +0800

    add oen

---
+++ splitArray.py
@@ -0,0 +1,21 @@
+from typing import *
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        start=max(nums)
+        end=sum(nums)
+        while start<end:
+            mid=(start+end)//2
+            count=1
+            s=0
+            for num in nums:
+                s+=num
+                if s>mid:
+                    count+=1
+                    s=num
+            if count>m:
+                start=mid+1
+            else:
+                end=mid
+        return start
+
