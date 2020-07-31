635c3a81bf6448a9d370fa037b9af9ec71df88ca
f4prime <zzz136454872@163.com>
2020-07-31 08:55:37 +0800

    add one

---
+++ findMagicIndex.py
@@ -0,0 +1,9 @@
+from typing import *
+
+class Solution:
+    def findMagicIndex(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i]==i:
+                return i
+        return -1
+
