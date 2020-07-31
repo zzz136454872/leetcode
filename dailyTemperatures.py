aac5f8b86449855863ca24704359c64455f89b7d
f4prime <zzz136454872@163.com>
2020-07-31 22:07:21 +0800

    add one

--- dailyTemperatures.py
+++ dailyTemperatures.py
@@ -1,6 +1,7 @@
 from typing import *
 
-class Solution:
+#hash map
+class Solution1:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         temp_day=[100000 for i in range(102)]
         out=[0 for i in T]
@@ -14,6 +15,23 @@ class Solution:
             else:
                 out[day]=0
         return out
+
+#Monotonous stack
+class Solution:
+    def dailyTemperatures(self,T):
+        stack=[]
+        out=[0 for i in T]
+        for day in range(len(T)-1,-1,-1):
+            temp=T[day]
+            while len(stack)>0 and stack[0][0]<=temp:
+                del stack[0]
+            if len(stack)==0:
+                out[day]=0
+            else:
+                out[day]=stack[0][1]-day
+            stack.insert(0,[temp,day])
+        return out
+
 sl=Solution()
 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
 out=sl.dailyTemperatures(temperatures)
