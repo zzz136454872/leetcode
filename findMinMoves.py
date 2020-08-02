92c6d16fa6abe3e5027e73c28ef35d6508b661ec
f4prime <zzz136454872@163.com>
2020-08-02 09:53:03 +0800

    add one

---
+++ findMinMoves.py
@@ -0,0 +1,23 @@
+from typing import *
+
+class Solution:
+    def findMinMoves(self, machines: List[int]) -> int:
+        if len(machines)==0:
+            return -1
+        total=sum(machines)
+        avg=total//len(machines)
+        if avg*len(machines)!=total:
+            return -1
+        machines=[i-avg for i in machines]
+        print(machines)
+        tmp=0
+        out=0
+        for m in machines:
+            tmp+=m
+            out=max(abs(tmp),out)
+        out=max(max(machines),out)
+        return out
+
+sl=Solution()
+m=[0,0,11,5]
+print(sl.findMinMoves(m))
