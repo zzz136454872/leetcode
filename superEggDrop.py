27b398a6105db96941e72ace92b7a35e1ed844ea
f4prime <zzz136454872@163.com>
2020-08-02 17:45:59 +0800

    update one

--- superEggDrop.py
+++ superEggDrop.py
@@ -4,7 +4,8 @@ class Solution:
     
     def superEggDrop(self, K: int, N: int) -> int:
         self.log=[[-1 for j in range(N+1)] for i in range(K+1)]
-        return self.count(K,N)
+        out=self.count(K,N)
+        return out
         
     def count(self,k,n):
         if n==0:
@@ -15,11 +16,22 @@ class Solution:
         if out!=-1:
             return out
         out=10000000
-        for i in range(1,n+1):
-            out=min(out,max(self.count(k-1,i-1),self.count(k,n-i))+1)
+        start=1
+        end=n
+        while start<=end:
+            mid=(start+end)//2
+            broke=self.count(k-1,n-mid)
+            not_broke=self.count(k,mid-1)
+            out=min(out,max(broke,not_broke)+1)
+            if broke==not_broke:
+                break
+            elif broke>not_broke:
+                start=mid+1
+            else:
+                end=mid-1
         self.log[k][n]=out
         return out
 
 sl=Solution()
-print(sl.superEggDrop(4,2000))
+print(sl.superEggDrop(2,9))
 
