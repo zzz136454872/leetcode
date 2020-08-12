8c9062e40013b863a907e94787f24ef9d779980a
f4prime <zzz136454872@163.com>
2020-08-12 09:34:40 +0800

    add one

---
+++ isPowerOfTwo.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<=0:
+            return False
+        tmp=1
+        while tmp<n:
+            tmp=tmp*2
+        if tmp==n:
+            return True
+        return False
+            
+
+sl=Solution()
+inp=1234
+print(sl.isPowerOfTwo(inp))
