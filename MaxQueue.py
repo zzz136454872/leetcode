642ec9cab56fb03f1408f497f36bd8ba2a5d2e04
f4prime <zzz136454872@163.com>
2020-08-18 09:28:40 +0800

    update one

--- MaxQueue.py
+++ MaxQueue.py
@@ -1,4 +1,4 @@
-class MaxQueue:
+class MaxQueue0:
 
     def __init__(self):
         self.max_int=-1
@@ -33,12 +33,48 @@ class MaxQueue:
                     self.max_count=self.queue.count(self.max_int)
             return out
 
+# q=MaxQueue()
+# print(q.push_back(1))
+# print(q.pop_front())
+# print(q.max_value())
+
+# 另一种解决方法
+class MaxQueue:
+    def __init__(self):
+        self.queue=[]
+        self.maxQueue=[]
+
+    def max_value(self) -> int:
+        if self.maxQueue:
+            return self.maxQueue[0]
+        else:
+            return -1
+
+    def push_back(self, value: int) -> None:
+        self.queue.append(value)
+        i=0
+        while i<len(self.maxQueue):
+            if self.maxQueue[i]<value:
+                del self.maxQueue[i]
+            else:
+                i+=1
+        self.maxQueue.append(value)
+
+    def pop_front(self) -> int:
+        if self.queue:
+            v=self.queue.pop(0)
+            if v==self.maxQueue[0]:
+                del self.maxQueue[0]
+        else:
+            v=-1
+        return v
+            
 q=MaxQueue()
-print(q.push_back(1))
-print(q.pop_front())
+print(q.push_back(15))
 print(q.max_value())
-
-
+print(q.push_back(9))
+print(q.max_value())
+#print(q.pop_front())
 
 # Your MaxQueue object will be instantiated and called as such:
 # obj = MaxQueue()
