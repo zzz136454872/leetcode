bbcdac5ae8dc4035d06f8f606d357aef254af40d
f4prime <zzz136454872@163.com>
2020-08-30 09:15:31 +0800

    add one

---
+++ reverseWords3.py
@@ -0,0 +1,10 @@
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        l=s.split()
+        l=[i[::-1] for i in l]
+        return ' '.join(l)
+
+sl=Solution()
+print(sl.reverseWords("Let's take LeetCode contest"))
