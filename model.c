68f86d2547f398740d933f6ffa5c6ea873b25ad3
f4prime <zzz136454872@163.com>
2020-09-07 21:31:04 +0800

    update model

--- model.c
+++ model.c
@@ -8,6 +8,19 @@
 #include<stdlib.h>
 #include<string.h>
 
+void print_int_star(int *list, int len)
+{
+    for(int i=0;i<len;i++)
+        printf("%d ",list[i]);
+    putchar('\n');
+}
+
+void print_int_2star(int **matrix,int row,int col)
+{
+    for(int i=0;i<row;i++)
+        print_int_star(matrix[i],col);
+}
+
 
 int main()
 {
