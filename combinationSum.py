d5f459427f627ee1c5466a061e0bc8835f67aa1f
f4prime <zzz136454872@163.com>
2020-09-09 08:38:46 +0800

    add one

--- combinationSum.py
+++ combinationSum.py
@@ -1,11 +1,27 @@
+from typing import *
+
 class Solution:
-    def combinationSum(self, candidates, target):
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        out=[]
+        candidates.sort()
+        def find(target,min_num):
+            out=[]
+            for num in candidates:
+                if num < min_num:
+                    continue
+                if target>num:
+                    tmp=find(target-num,num)
+                    for l in tmp:
+                        out.append([num]+l)
+                elif target==num:
+                    out.append([num])
+                else:
+                    break
+            return out
+        return find(target,-1)
 
-inp=[10,1,2,7,6,1,5]
+candidates = [2,3,6,7]
+target = 7
 sl=Solution()
-
-print(sl.combinationSum(inp,8))
+print(sl.combinationSum(candidates,target))
         
-
-
-            
