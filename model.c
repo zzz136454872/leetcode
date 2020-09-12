f679ec021849bfb3db5d91357d78228a15ebe730
f4prime <zzz136454872@163.com>
2020-09-12 10:33:52 +0800

    update model

--- model.c
+++ model.c
@@ -21,11 +21,31 @@ void print_int_2star(int **matrix,int row,int col)
         print_int_star(matrix[i],col);
 }
 
+void print_double_star(double *list, int len)
+{
+    for(int i=0;i<len;i++)
+        printf("%.2lf ",list[i]);
+    putchar('\n');
+}
+
 #define max(a,b) (((a)>(b))?(a):(b))
+
+//#define testMod
+#ifdef testMod
+void test() 
+{
+
+}
+#endif
 
 int main()
 {
+#ifdef testMod
+    test();
+#endif
+#ifndef testMod
 
+#endif
     return 0;
 }
     
