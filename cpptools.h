89122a3baa8c56494cb273718a2febff24c5b1c4
f4prime <zzz136454872@163.com>
2020-09-14 16:45:13 +0800

    add cpptools

---
+++ cpptools.h
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+//显示中间结果
+void print(vector<int> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+
+void print(vector<vector<int>> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+    {
+        for(int j=0;j<(int)a[i].size();j++)
+            cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+}
+
+void print(vector<bool> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+    {
+        cout<<i<<" ";
+        if(a[i])
+            cout<<"true ";
+        else
+            cout<<"false ";
+    }
+    cout<<endl;
+}
+
+void print(bool a[],int len)
+{
+    for(int i=0;i<len;i++)
+    {
+        cout<<i<<" ";
+        if(a[i])
+            cout<<"true ";
+        else
+            cout<<"false ";
+    }
+    cout<<endl;
+}
+
+void print(vector<string> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+        cout<<a[i]<<endl;
+}
+
+void print(vector<vector<string>> a)
+{
+    for(int i=0;i<(int)a.size();i++)
+    {
+        print(a[i]);
+        cout<<endl;
+    }
+}
--- model.cpp
+++ model.cpp
@@ -3,7 +3,7 @@
  * @email zzz136454872@163.com
  * @aim a plain model for leetcode
  */
-
+#include"cpptools.h"
 #include<iostream>
 #include<string>
 #include<vector>
@@ -13,65 +13,6 @@
 #include<queue>
 
 using namespace std;
-
-//显示中间结果
-void print(vector<int> a)
-{
-    for(int i=0;i<(int)a.size();i++)
-        cout<<a[i]<<" ";
-    cout<<endl;
-}
-
-void print(vector<vector<int>> a)
-{
-    for(int i=0;i<(int)a.size();i++)
-    {
-        for(int j=0;j<(int)a[i].size();j++)
-            cout<<a[i][j]<<" ";
-        cout<<endl;
-    }
-}
-
-void print(vector<bool> a)
-{
-    for(int i=0;i<(int)a.size();i++)
-    {
-        cout<<i<<" ";
-        if(a[i])
-            cout<<"true ";
-        else
-            cout<<"false ";
-    }
-    cout<<endl;
-}
-
-void print(bool a[],int len)
-{
-    for(int i=0;i<len;i++)
-    {
-        cout<<i<<" ";
-        if(a[i])
-            cout<<"true ";
-        else
-            cout<<"false ";
-    }
-    cout<<endl;
-}
-
-void print(vector<string> a)
-{
-    for(int i=0;i<(int)a.size();i++)
-        cout<<a[i]<<endl;
-}
-
-void print(vector<vector<string>> a)
-{
-    for(int i=0;i<(int)a.size();i++)
-    {
-        print(a[i]);
-        cout<<endl;
-    }
-}
 
 //#define testMod
 
@@ -90,9 +31,6 @@ public:
         
 private:
 };
-
-
-
 #endif
 
 int main()
@@ -103,7 +41,6 @@ int main()
 
 #ifndef testMod
     Solution sl;
-
 
     
 #endif 
