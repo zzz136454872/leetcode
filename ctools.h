945d16b40c26c1248f502890439ad365fb2dfd4d
f4prime <zzz136454872@163.com>
2020-09-14 16:41:41 +0800

    add ctools.h

---
+++ ctools.h
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void print_int_star(int *list, int len)
+{
+    for(int i=0;i<len;i++)
+        printf("%d ",list[i]);
+    putchar('\n');
+}
+
+void print_int_2star(int **matrix,int row,int col)
+{
+    for(int i=0;i<row;i++)
+        print_int_star(matrix[i],col);
+}
+
+void print_double_star(double *list, int len)
+{
+    for(int i=0;i<len;i++)
+        printf("%.2lf ",list[i]);
+    putchar('\n');
+}
+
+#define max(a,b) (((a)>(b))?(a):(b))
--- model.c
+++ model.c
@@ -4,31 +4,10 @@
  * @aim a plain model for leetcode
  */
 
+#include"ctools.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-void print_int_star(int *list, int len)
-{
-    for(int i=0;i<len;i++)
-        printf("%d ",list[i]);
-    putchar('\n');
-}
-
-void print_int_2star(int **matrix,int row,int col)
-{
-    for(int i=0;i<row;i++)
-        print_int_star(matrix[i],col);
-}
-
-void print_double_star(double *list, int len)
-{
-    for(int i=0;i<len;i++)
-        printf("%.2lf ",list[i]);
-    putchar('\n');
-}
-
-#define max(a,b) (((a)>(b))?(a):(b))
 
 //#define testMod
 #ifdef testMod
