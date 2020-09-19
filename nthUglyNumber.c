bd6ac3f763af9b11ef7da207adf8fb8a964a9964
f4prime <zzz136454872@163.com>
2020-09-19 16:36:26 +0800

    add one

--- nthUglyNumber.c
+++ nthUglyNumber.c
@@ -22,8 +22,9 @@ void test()
 #ifndef testMod
 
 #define min(a,b) (((a)<(b))?(a):(b))
-int nthUglyNumber(int n){
-    int* table=(int*)malloc(sizeof(int)*(n+1));
+//丑数II
+int nthUglyNumberII(int n){
+    int table[1691];
     table[0]=0;
     table[1]=1;
     int a=1,b=1,c=1;
@@ -38,9 +39,43 @@ int nthUglyNumber(int n){
         if(table[i]==table[c]*5)
             c++;
     }
-    int out=table[n];
-    free(table);
-    return out;
+    return table[n];
+}
+
+long long leastCommonMultiple(long long a, long long b)
+{
+    long long tmp,aa=a,bb=b;
+    while(bb!=0)
+    {
+        tmp=bb;
+        bb=aa%bb;
+        aa=tmp;
+    }
+    return a*b/aa;
+}
+
+//丑数III
+int nthUglyNumber(int n, int a, int b, int c){
+    long long right=1000000000000000000+11;
+    long long left=1;
+    long long ab=leastCommonMultiple(a,b);
+    long long ac=leastCommonMultiple(a,c);
+    long long bc=leastCommonMultiple(b,c);
+    long long abc=leastCommonMultiple(ab,c);
+    long long mid;
+    long long sum;
+    while(left<right)
+    {
+        mid=(left+right)/2;
+        sum=mid/a+mid/b+mid/c-mid/ab-mid/ac-mid/bc+mid/abc;
+        if(sum<n)
+            left=mid+1;
+        else if(sum>n)
+            right=mid-1;
+        else
+            right=mid;
+    }
+    return (int)left;
 }
 
 #endif
@@ -51,7 +86,9 @@ int main()
     test();
 #endif
 #ifndef testMod
-
+    int n=4,a=2,b=3,c=4;
+    int out=nthUglyNumber(n,a,b,c);
+    printf("%d\n",out);
 #endif
     return 0;
 }
