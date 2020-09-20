335620c029e448aa5249c687c770a3c262bc2278
f4prime <zzz136454872@163.com>
2020-09-20 16:17:35 +0800

    add one

--- cuttingRope.c
+++ cuttingRope.c
@@ -21,7 +21,8 @@ void test()
 #define min(a,b) (((a)<(b))?(a):(b))
 
 #ifndef testMod
-int cuttingRope(int n){
+//剪绳子
+int cuttingRope0(int n){
     if(n<=4)
     {
         switch(n)
@@ -50,6 +51,38 @@ int cuttingRope(int n){
     }
 }
 
+//剪绳子II
+int cuttingRope(int n){
+    if(n<=4)
+    {
+        switch(n)
+        {
+            case 2:
+                return 1;
+            case 3:
+                return 2;
+            case 4:
+                return 4;
+            default:
+                return -1;
+        }
+    }
+    long long base=1;
+    for(int i=0;i<n/3-1;i++)
+        base=(3*base)%1000000007;
+    switch(n%3)
+    {
+        case 0:
+            return (3*base)%1000000007;
+        case 1:
+            return (4*base)%1000000007;
+        case 2:
+            return (6*base)%1000000007;
+        default:
+            printf("error in switch\n");
+            exit(-1);
+    }
+}
 #endif
 
 int main()
