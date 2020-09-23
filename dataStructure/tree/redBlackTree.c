ffbc7a9e34f2906e8f1feb7851f68b8cb722b867
f4prime <zzz136454872@163.com>
2020-09-23 16:41:23 +0800

    add one

--- dataStructure/tree/redBlackTree.c
+++ dataStructure/tree/redBlackTree.c
@@ -55,12 +55,13 @@ void addRight(RBTNode* root,int val)
 //将返回节点的parent设为NULL
 RBTNode* leftAdjust(RBTNode* root)
 {
-    RBTNode* newRoot=root->r;
-    RBTNode *tmp=newRoot->l;
-    newRoot->l=root; 
+    RBTNode* newRoot=root->right;
+    RBTNode *tmp=newRoot->left;
+    newRoot->left=root; 
     root->parent=newRoot;
-    root->r=tmp;
-    tmp->parent=root;
+    root->right=tmp;
+    if(tmp!=NULL)
+        tmp->parent=root;
     newRoot->parent=NULL;
     return newRoot;
 }
@@ -71,13 +72,95 @@ RBTNode* leftAdjust(RBTNode* root)
 //将返回节点的parent设为NULL
 RBTNode* rightAdjust(RBTNode* root)
 {
-    RBTNode* newRoot=root->l;
-    RBTNode* tmp=newRoot->r;
-    newRoot->r=root;
+    RBTNode* newRoot=root->left;
+    RBTNode* tmp=newRoot->right;
+    newRoot->right=root;
     root->parent=newRoot;
-    root->l=tmp;
-    tmp->parent=root;
+    root->left=tmp;
+    if(tmp!=NULL)
+        tmp->parent=root;
     return newRoot;
+}
+
+RBTNode* balanceRBT(RBTNode* newNode,RBTNode* root)
+{
+    RBTNode* parent=newNode->parent;
+    if(parent==NULL)
+    {
+        newNode->color=BLACK;
+        return newNode;
+    }
+    if(parent->color==BLACK)
+        return root;//不需要调整
+    RBTNode* grand=parent->parent;
+    RBTNode* uncle=grand->left;
+    RBTNode* greatgrand=grand->parent;
+    if(uncle==parent)
+        uncle=grand->right;
+    if(uncle!=NULL&&uncle->color==RED)
+    {
+        grand->color=RED;
+        uncle->color=BLACK;
+        parent->color=BLACK;
+        //这里有可能导致根节点为红色
+        //将parent作为新的节点进行处理
+        return balanceRBT(grand,root);
+    }
+    else
+    {
+        if(parent==grand->left)
+        {
+            if(newNode==parent->right)
+            {
+                grand->left=leftAdjust(parent);
+                grand->left->parent=grand;
+                parent=grand->left;
+                newNode=parent->left;
+            }
+            grand=rightAdjust(grand);
+            if(greatgrand!=NULL)
+            {
+                grand->parent=greatgrand;
+                if(greatgrand->left==grand->right)
+                    greatgrand->left=grand;
+                else
+                    greatgrand->right=grand;
+            }
+            grand->right->color=RED;
+            grand->color=BLACK;
+            if(greatgrand!=NULL)
+                return root;
+            else
+                return grand;
+        }
+        else
+        {
+            if(newNode==parent->left)
+            {
+                grand->right=rightAdjust(parent);
+                grand->right->parent=grand;
+                parent=grand->right;
+                newNode=parent->right;
+            }
+            grand=leftAdjust(grand);
+            if(greatgrand!=NULL)
+            {
+                grand->parent=greatgrand;
+                if(greatgrand->left==grand->left)
+                    greatgrand->left=grand;
+                else
+                    greatgrand->right=grand;
+            }
+            grand->left->color=RED;
+            grand->color=BLACK;
+            if(greatgrand!=NULL)
+                return root;
+            else
+                return grand;
+        }
+    }
+    assert(false);//will not go there
+    return NULL;
 }
 
 //插入的过程中，所有左子树中的节点小于当前
@@ -110,25 +193,132 @@ RBTNode* insert(RBTNode* root, int val)
         }
     }
     if(p->val>val)//插入左子节点
-    {
         p->left=newNode;
-        newNode->parent=p;
-        if(p->color=BLACK)
-            return;//不需要调整
-    }
     else//插入右子节点
-    {
         p->right=newNode;
-        newNode->parent=p;
-        if(p->color=BLACK)
-            return;//不需要调整
-    }
-    RBTNode* grand=p->parent;
-    RBTNode* uncle=grand->left;
-    if(uncle==p)
-        uncle=grand->right;
-    if(uncle==NULL)
+    newNode->parent=p;
+    return balanceRBT(newNode,root);
+}
+
+//删除节点进行平衡的函数
+RBTNode* deleteBalance(p,root)
+{
+    assert(p!=NULL);
+    RBTNode *parent=p->parent;
+    if(parent==NULL)
         return;
+    RBTNode* uncle;
+    RBTNode* grand;
+    RBTNode* nephewLeft, nephewRight;
+    if(parent->left==p) //p是parent的左子节点
+    {
+        uncle=parent->right;
+        if(uncle!=NULL&&uncle->color=RED)
+        {
+            grand=parent->parent;
+            parent->color=RED;
+            uncle->color=BLACK;
+            parent=leftAdjust(parent);
+            if(grand!=NULL)
+            {
+                parent->parent=grand;
+                if(parent->left==grand->left)
+                    grand->left=parent;
+                else
+                    grand->right=parent;
+            }
+            return deleteBalance(p,root);
+        }
+        else
+        {
+            if(uncle!=NULL&&uncle->right->color==RED)
+            {
+                uncle->color=parent->color;
+                parent->color=BLACK;
+                nephewRight=uncle->right;
+                if(nephewLeft->color!=
+                        //TODO add code here
+            }
+            else if(uncle!=NULL&&uncle->left->color==RED)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+    else
+    {
+
+    }
+}
+
+//查找一个节点的后继节点
+RBTNode* findNext(RBTNode* node)
+{
+    while(node!=NULL&&node->parent->right==node)
+        node==node->parent;
+    if(node==NULL)
+        return NULL;
+    node=node->right;
+    while(node->left!=NULL)
+        node=node->left;
+    return node;
+}
+
+//查找删除节点的替代节点
+RBTNode* findReplace(RBTNode* node)
+{
+    if(node->left==NULL&&node->right==NULL)
+        return node;
+    if(node->left==NULL)
+        return findReplace(node->right);
+    if(node->right==NULL)
+        return findReplace(node->left);
+    return findReplace(findNext(node));
+}
+
+//删除节点的函数
+RBTNode* delete(RBTNode* root,int val)
+{
+    RBTNode* p=root;
+    while(p!=NULL&&p->val!=val)
+    {
+        if(p->val>val)
+            p=p->left;
+        else
+            p=p->right;
+    }
+    if(p==NULL) //没有要删除的数值
+        return root;
+    //使用删除replaceNode替代删除p
+    RBTNode* replaceNode=findReplace(p);
+    int tmp=replaceNode->val;
+    replaceNode->val=p->val;
+    p->val=tmp;
+    p=replaceNode;
+    RBTNode* parent=p->parent;
+    if(p->color==RED) //p是红色的节点，删除不破坏平衡
+    {
+        if(parent!=NULL) //将parent指向p的指针变为null
+        {
+            if(parent->left==p)
+                parent->left=NULL;
+            else
+                parent->right=NULL;
+        }
+        free(p);
+    }
+    RBTNode*out=deleteBalance(p,root);
+    if(out->parent!=NULL)
+    {
+        if(out==parent->left)
+            parent->left=NULL;
+        else
+            parent->right=NULL;
+    }
+    free(p);
+    return out;
 }
 
 //递归判断是否每个叶节点序列中的
@@ -186,6 +376,35 @@ bool checkRBTSub(RBTNode* root)
     return true;
 }
 
+//检查每一个节点的左右子节点的parent
+//指针是不是都指向自己。
+bool checkParent(RBTNode* root)
+{
+    if(root==NULL)
+        return true;
+    if(root->left!=NULL)
+    {
+        if(root->left->parent!=root)
+        {
+            printf("%d -> %d parent error\n",root->val,root->left->val);
+            return false;
+        }
+        if(!checkParent(root->left))
+            return false;
+    }
+    if(root->right!=NULL)
+    {
+        if(root->right->parent!=root)
+        {
+            printf("%d -> %d parent error\n",root->val, root->right->val);
+            return false;
+        }
+        if(!checkParent(root->right))
+            return false;
+    }
+    return true;
+}
+
 //检查是不是合法的红黑树的函数
 //bool 定义在 ctool.h中
 bool checkRBT(RBTNode* root)
@@ -193,29 +412,71 @@ bool checkRBT(RBTNode* root)
     if(root==NULL)
         return true;
     if(root->color!=BLACK)
+    {
+        printf("root is not black\n");
         return false;
+    }
+    if(!checkParent(root))
+    {
+        printf("parent error\n");
+        return false;
+    }
     if(!blackBalance(root))
     {
-        printf("black");
+        printf("black path error\n");
         return false;
     }
     if(!checkRBTSub(root))
     {
-        printf("sub");
+        printf("red child error\n");
         return false;
     }
     return true;
 }
 
+//输出红黑树的结构的函数
+void printRBT(RBTNode* root,int depth)
+{
+    if(root==NULL||depth>3)
+        return;
+    printf("node: %d color: ",root->val);
+    if(root->color==BLACK)
+        printf("black ");
+    else
+        printf("red");
+    if(root->parent!=NULL)
+        printf(" parent: %d ",root->parent->val);
+    else
+        printf("parent: null "); 
+    if(root->left!=NULL)
+        printf("left: %d ",root->left->val);
+    else
+        printf("left: null ");
+    if(root->right!=NULL)
+        printf("right: %d\n",root->right->val);
+    else
+        printf("right: null\n");
+    printRBT(root->left,depth+1);
+    printRBT(root->right,depth+1);
+}
+
 int main()
 {
-    RBTNode* root=getRBTNode(1);
-    root->color=BLACK;
-    addLeft(root,0);
-    addRight(root,100);
-    addRight(root->right,100);
-    root->left->color=BLACK;
-    root->right->right->color=BLACK;
+    RBTNode* root=NULL;
+    root=insert(root,1);
+    root=insert(root,2);
+    root=insert(root,3);
+    root=insert(root,4);
+    root=insert(root,5);
+    root=insert(root,14);
+    root=insert(root,12);
+    root=insert(root,15);
+    root=insert(root,8);
     printf("%d\n",checkRBT(root));
+    printRBT(root,0);
+    root=insert(root,9);
+    printf("\n\n");
+    printf("%d\n",checkRBT(root));
+    printRBT(root,0);
     return 0;
 }
---
+++ longestPalindromeSubseq.cpp
@@ -0,0 +1,72 @@
+/**
+ * @author f4prime
+ * @email zzz136454872@163.com
+ * @aim a plain model for leetcode
+ */
+#include"cpptools.h"
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+//#define testMod
+
+#ifdef testMod
+void test()
+{
+
+    
+}
+#endif
+
+#ifndef testMod
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        if(s.size()==0)
+            return 0;
+        table=vector<vector<int>>(s.size(),vector<int>(s.size(),0));
+        this->s=s;
+        return find(0,(int)s.size()-1);
+    }
+
+    int find(int begin,int end)
+    {
+        if(begin>end)
+            return 0;
+        if(begin==end)
+            return 1;
+        if(table[begin][end]!=0)
+            return table[begin][end];
+        int maxLen=max(find(begin+1,end),find(begin,end-1));
+        if(s[begin]==s[end])
+            maxLen=max(maxLen,find(begin+1,end-1)+2);
+        table[begin][end]=maxLen;
+        return maxLen;
+    }
+
+private:
+    vector<vector<int>> table;
+    string s;
+};
+#endif
+
+int main()
+{
+#ifdef testMod
+    test();
+#endif
+
+#ifndef testMod
+    Solution sl;
+    string s="bbbab";
+    cout<<sl.longestPalindromeSubseq(s)<<endl;
+#endif 
+    return 0;
+}
+
