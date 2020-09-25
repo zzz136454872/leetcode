30b6d1a6d469c783362b58d12b27367c0337de87
f4prime <zzz136454872@163.com>
2020-09-25 09:09:01 +0800

    add one

--- buildTree.py
+++ buildTree.py
@@ -13,7 +13,7 @@ def printTree(root):
         printTree(root.left)
         printTree(root.right)
 
-class Solution:
+class Solution1: #前序遍历和中序遍历
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if(len(preorder)==0):
             return None
@@ -22,8 +22,20 @@ class Solution:
         root.left=self.buildTree(preorder[1:mid+1],inorder[0:mid])
         root.right=self.buildTree(preorder[mid+1:],inorder[mid+1:])
         return root
-preorder = [3,9,20,15,7]
+
+class Solution: #中序遍历和后序遍历
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if(len(inorder)==0):
+            return None
+        root=TreeNode(postorder[-1])
+        #print(root.val)
+        mid=inorder.index(root.val)
+        root.left=self.buildTree(inorder[:mid],postorder[:mid])
+        root.right=self.buildTree(inorder[mid+1:],postorder[mid:len(postorder)-1])
+        return root
+
+postorder = [9,15,7,20,3]
 inorder = [9,3,15,20,7]
 sl=Solution()
-printTree(sl.buildTree(preorder,inorder))
+printTree(sl.buildTree(inorder,postorder))
 
