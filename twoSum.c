83f2835f96b4221a17b9ca169a299d0a78610402
f4prime <zzz136454872@163.com>
2020-10-04 09:00:03 +0800

    add one

--- twoSum.c
+++ twoSum.c
@@ -3,8 +3,9 @@
  */
 #include<stdio.h>
 #include<stdlib.h>
+#include"ctools.h"
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+int* twoSum0(int* nums, int numsSize, int target, int* returnSize){
     int i;
     int j;
     int *out = (int*)malloc(2*sizeof(int));
@@ -21,18 +22,56 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     return out;
 }
 
+typedef struct {
+    int val;
+    int loc;
+}pair;
+
+int cmp(const void *a, const void* b)
+{
+    return ((pair*)a)->val-((pair*)b)->val;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    pair* table=(pair*)malloc(sizeof(pair)* numsSize);
+    int i;
+    for(i=0;i<numsSize;i++)
+    {
+        table[i].val=nums[i];
+        table[i].loc=i;
+    }
+    qsort(table,numsSize, sizeof(pair),cmp);
+    *returnSize=2;
+    int j=numsSize-1;
+    i=0;
+    int *out=(int*)malloc(sizeof(int)*2);
+    int tmp;
+    while(i<j)
+    {
+        tmp=table[i].val+table[j].val;
+        if(tmp==target)
+        {
+            out[0]=table[i].loc;
+            out[1]=table[j].loc;
+            break;
+        }
+        else if(tmp>target)
+            j--;
+        else
+            i++;
+    }
+    free(table);
+    return out;
+}
+
 int main()
 {
     const int size=4;
     int nums[4]={1,3,100,-1};
     int target=101;
-    int *out;
     int ret_size;
-    out=twoSum(nums,size,target,&ret_size);
-    int i;
-    printf("now\n");
-    for(i=0;i<ret_size;i++)
-        printf("%d, ",out[i]);
+    int* out=twoSum(nums,size,target,&ret_size);
+    print_int_star(out, ret_size);
     free(out);
     return 0;
 }
