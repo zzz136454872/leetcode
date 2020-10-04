7df3671e78e2100fd08df4a75eb8e01d29a9666e
f4prime <zzz136454872@163.com>
2020-10-04 09:00:19 +0800

    add clist

---
+++ clist.h
@@ -0,0 +1,30 @@
+#ifndef clist_h
+#define clist_h
+#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+}
+
+struct ListNode* getListNode(int val)
+{
+    struct ListNode* out=(struct ListNode*)malloc(sizeof(struct ListNode));
+    out->val=val;
+    out->next=NULL;
+    return out;
+}
+
+void travelList(struct ListNode* head)
+{
+    if(head==NULL)
+    {
+        putchar('\n');
+        return;
+    }
+    printf("%d\n",head->val);
+    travelList(head->next);
+}
+
+#endif
