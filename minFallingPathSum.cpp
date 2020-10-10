027d2a4381a404b9d29237de99dfcdb2a4d9cfe7
f4prime <zzz136454872@163.com>
2020-10-10 23:17:35 +0800

    add one

--- minFallingPathSum.cpp
+++ minFallingPathSum.cpp
@@ -24,7 +24,38 @@ void test()
 #endif
 
 #ifndef testMod
+//下降路径最小和 
 class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& A) {
+        int n=(int)A.size();
+        vector<vector<int>> table=vector<vector<int>>(n,vector<int>(n,0));
+        for(int j=0;j<n;j++)
+            table[0][j]=A[0][j];
+        int tmp=12345;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                tmp=123456;
+                if(j>0)
+                    tmp=min(tmp,table[i-1][j-1]);
+                tmp=min(tmp,table[i-1][j]);
+                if(j<n-1)
+                    tmp=min(tmp,table[i-1][j+1]);
+                table[i][j]=tmp+A[i][j];
+            }
+        }
+        tmp=123456;
+        for(int j=0;j<n;j++)
+            tmp=min(tmp,table[n-1][j]);
+        return tmp;
+    }
+private:
+};
+
+//下降路径最小和II
+class Solution2 {
 public:
     int minFallingPathSum(vector<vector<int>>& arr) {
         int n=(int)arr.size();
