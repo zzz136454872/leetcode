9b0fb7a72d6c017e95fe8a92f45f0b68b942ae97
f4prime <zzz136454872@163.com>
2020-10-23 09:01:20 +0800

    add one

--- isPalindrome.c
+++ isPalindrome.c
@@ -2,15 +2,17 @@
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
+#include"clist.h"
+#include"ctools.h"
 
-bool isPalindrome(int x){
+bool isPalindrome1(int x){
     if(x < 0)
         return false;
     if(x == 0)
         return true;
     char tmp[20]={0};
     int i=0;
-    while
+    while(i>0)
     {
         tmp[i++]=x % 10 + '0';
         x/=10;
@@ -27,10 +29,47 @@ bool isPalindrome(int x){
     return false;
 }
 
+//回文链表
+bool isPalindrome(struct ListNode* head){
+    int bufferSize=2;
+    int *buffer=(int*)malloc(sizeof(int)*bufferSize);
+    struct ListNode* p;
+    p=head;
+    int i=0;
+    while(p!=NULL)
+    {
+        if(i==bufferSize)
+        {
+            bufferSize*=2;
+            buffer=(int*)realloc(buffer,sizeof(int)*bufferSize);
+        }
+        buffer[i++]=p->val;
+        p=p->next;
+    }
+    int j=i-1;
+    i=0;
+    bool out=true;
+    while(i<j)
+    {
+        if(buffer[i]!=buffer[j])
+        {
+            out=false;
+            break;
+        }
+        i++;
+        j--;
+    }
+    return out;
+}
+
 int main()
 {
-    char a[]="1234321";
-    int b=isPalindrome(a);
-    printf("%d\n",b);
+    //char a[]="1234321";
+    //int b=isPalindrome(a);
+    struct ListNode* head=getListNode(1);
+    head->next=getListNode(2);
+    head->next->next=getListNode(2);
+    head->next->next->next=getListNode(1);
+    printf("%d\n",isPalindrome(head));
     return 0;
 }
