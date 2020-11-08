e83d7b86392a7d87c928ff0e16cdaa365092f172
f4prime <zzz136454872@163.com>
2020-11-08 10:00:08 +0800

    add one

--- maxProfit.py
+++ maxProfit.py
@@ -1,4 +1,7 @@
-class Solution:
+from typing import *
+
+# 买卖股票的最佳时机
+class Solution1:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = 100000000
         profit=0
@@ -7,5 +10,24 @@ class Solution:
             profit=max(profit,price-min_price)
         return profit
 
+# 买卖股票的最佳时机II
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        preMin=100000
+        last=100000
+        out=0
+        for i in range(len(prices)):
+            if prices[i]>last:
+                last=prices[i]
+                continue
+            out+=last-preMin
+            last=prices[i]
+            preMin=last
+        out+=last-preMin
+        return out
 
         
+sl=Solution()
+prices=[7,1,5,3,6,4]
+print(sl.maxProfit(prices))
