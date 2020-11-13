4a8b62cc7b1172bfe77ac9844f375eb23cb51aad
f4prime <zzz136454872@163.com>
2020-11-13 15:35:27 +0800

    add one

--- maxProfit.py
+++ maxProfit.py
@@ -12,7 +12,7 @@ class Solution1:
 
 # 买卖股票的最佳时机II
 
-class Solution:
+class Solution2:
     def maxProfit(self, prices: List[int]) -> int:
         preMin=100000
         last=100000
@@ -27,7 +27,48 @@ class Solution:
         out+=last-preMin
         return out
 
-        
+# 销售价值减少的颜色球
+
+class Solution:
+    def maxProfit(self, inventory: List[int], orders: int) -> int:
+        if orders==0:
+            return 0
+        inventory.sort(reverse=True)
+        inventory.append(0)
+        count=0
+        for i in range(len(inventory)):
+            count+=(i+1)*(inventory[i]-inventory[i+1])
+            if count>=orders:
+                height=inventory[i+1]
+                more=count-orders
+                columns=i+1
+                break
+        out=0
+        more_height=more//columns
+        height+=more_height
+        more=more%columns
+        # print(more,height)
+
+        for c in inventory:
+            if c<=height:
+                break
+            out+=(c+height+1)*(c-height)//2
+
+        out-=more*(height+1)
+        return out%(10**9+7)
+
+# sl=Solution()
+# prices=[7,1,5,3,6,4]
+# print(sl.maxProfit(prices))
+
+inventory = [2,8,4,10,6]
+orders = 20
+inventory = [1000000000]
+orders = 1000000000
 sl=Solution()
-prices=[7,1,5,3,6,4]
-print(sl.maxProfit(prices))
+print(sl.maxProfit(inventory,orders))
+            
+
+
+
+        
