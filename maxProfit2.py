6ccb4c65382fb5decd38720f47df6be2a890b7ef
f4prime <zzz136454872@163.com>
2020-12-17 09:03:20 +0800

    add one

--- maxProfit2.py
+++ maxProfit2.py
@@ -16,7 +16,24 @@ class Solution:
             unhold[i]=max(unhold[i-1],hold[i]+prices[i])
 
         return unhold[-1]
+
+# 买卖股票的最佳时机含手续费
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        days=len(prices)
+        if days<=1:
+            return 0
+        hold=-prices[0]
+        unhold=0
+        for i in range(1,days):
+            next_hold=max(hold,unhold-prices[i])
+            next_unhold=max(unhold, hold+prices[i]-fee)
+            hold=next_hold
+            unhold=next_unhold
+        return unhold
      
 sl=Solution()
-prices=[1,2,3,0,2]
-print(sl.maxProfit(prices))
+# prices=[1,2,3,0,2]
+prices = [1, 3, 2, 8, 4, 9]
+fee = 2
+print(sl.maxProfit(prices,fee))
