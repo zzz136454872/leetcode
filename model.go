405f8c52020338efdf97defa634f89f213712ccf
f4prime <zzz136454872@163.com>
2020-12-24 09:23:27 +0800

    add one

--- model.go
+++ model.go
@@ -6,6 +6,11 @@ import(
     //"sort"
 )
 
+var testMod=false;
+
+func test() {
+}
+
 func int_min(inp... int) int {
     out:=inp[0]
     for _,v := range inp {
@@ -16,9 +21,22 @@ func int_min(inp... int) int {
     return out
 }
 
-var testMod=false;
+func int_max(inp... int) int {
+    out:=inp[0]
+    for _,v := range inp {
+        if out<v {
+            out=v
+        }
+    }
+    return out
+}
 
-func test() {
+func int_sum(inp []int) int {
+    out:=0
+    for _,v := range inp {
+        out+=v
+    }
+    return out
 }
 
 func main() {
