c4c26109ce501346a8f3765ea7ecc81b08b9bd70
f4prime <zzz136454872@163.com>
2020-12-30 20:54:05 +0800

    add one

--- model.go
+++ model.go
@@ -21,16 +21,6 @@ func int_min(inp... int) int {
     return out
 }
 
-func int_max(inp... int) int {
-    out:=inp[0]
-    for _,v := range inp {
-        if out<v {
-            out=v
-        }
-    }
-    return out
-}
-
 func int_sum(inp []int) int {
     out:=0
     for _,v := range inp {
@@ -39,7 +29,62 @@ func int_sum(inp []int) int {
     return out
 }
 
+func max(inp ...interface{}) interface{}{
+    if len(inp)==1 {
+        list:=inp[0]
+        switch list.(type) {
+        case []int:
+            out:=list.([]int)[0]
+            for _,v := range list.([]int)[1:] {
+                if out<v {
+                    out=v
+                }
+            }
+            return out
+        case []float32:
+            out:=list.([]float32)[0]
+            for _,v := range list.([]float32)[1:] {
+                if out<v {
+                    out=v
+                }
+            }
+            return out
+        case []float64:
+            out:=list.([]float64)[0]
+            for _,v := range list.([]float64)[1:] {
+                if out<v {
+                    out=v
+                }
+            }
+            return out
+        default:
+            fmt.Printf("type unknown: %T\n",list)
+        }
+    }
+    out:=inp[0]
+    for _,v := range inp {
+        switch out.(type) {
+        case int:
+            if out.(int)<v.(int) {
+                out=v
+            }
+        case float32:
+            if out.(float32)<v.(float32) {
+                out=v
+            }
+        case float64:
+            if out.(float64)<v.(float64) {
+                out=v
+            }
+        default:
+            fmt.Printf("type unknown: %T\n",out)
+            return out
+        }
+    }
+    return out
+}
+
 func main() {
     test()
-    fmt.Printf("%d\n",1);
+    fmt.Println(max([]int{1,2}))
 }
