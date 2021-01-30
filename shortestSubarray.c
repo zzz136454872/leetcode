88252cdad3c07b94eb9727b16e7bcbf20f3e97e3
f4prime <zzz136454872@163.com>
2021-01-30 10:43:40 +0800

    add one

--- shortestSubarray.c
+++ shortestSubarray.c
@@ -4,8 +4,6 @@
  * @aim a plain model for leetcode
  */
 
-// wrong answer
-
 #include"ctools.h"
 #include<stdio.h>
 #include<stdlib.h>
@@ -23,31 +21,81 @@ void test()
 #define min(a,b) (((a)<(b))?(a):(b))
 
 #ifndef testMod
-int shortestSubarray(int* A, int ASize, int K){
-    int begin,end;
-    begin=0;
-    end=0;
-    int sum=0;
-    int minLen=123456;
-    while(end<ASize||sum>=K)
+typedef struct {
+    int idx,v;
+} node;
+
+node* stack;
+int size;
+int num;
+
+void push(node a) 
+{
+    if(num==size)
     {
-        printf("%d %d %d %d\n",begin,end,sum,minLen);
-        if(sum<0)
-        {
-            begin=end;
-            sum=0;
-        }
-        else if(sum<K)
-        {
-            sum+=A[end++];
-        }
-        else if(sum>=K)
-        {
-            minLen=min(minLen,end-begin);
-            sum-=A[begin++];
-        }
+        size*=2;
+        stack=realloc(stack,sizeof(node)*size);
     }
-    return minLen>12345?-1:minLen;
+    stack[num++]=a;
+}
+
+node pop() 
+{
+    return stack[--num];
+}
+
+int bs(int a)
+{
+    int start=0;
+    int end=num-1;
+    int mid=0;
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        if(stack[mid].v>a)
+            end=mid-1;
+        else
+            start=mid+1;
+    }
+    return start;
+}
+
+int shortestSubarray(int* A, int ASize, int K){
+    size=2;
+    stack=(node*)malloc(sizeof(node)*size);
+    num=0;
+    int* pre_sum=(int*)malloc(sizeof(int)*(ASize+1));
+    memset(pre_sum,0,sizeof(int)*(ASize+1));
+    int tmp=0;
+    int i;
+    for(i=0;i<ASize;i++)
+    {
+        tmp+=A[i];
+        pre_sum[i+1]=tmp;
+    }
+    node n1;
+    n1.v=0;
+    n1.idx=0;
+    push(n1);
+    int out=12345678;
+    for(i=1;i<ASize+1;i++)
+    {
+        int loc=bs(pre_sum[i]-K);
+        if(loc>0)
+        {
+            out=min(out,i-stack[loc-1].idx);
+        }
+        while(num>0&&stack[num-1].v>=pre_sum[i])
+        {
+            pop();
+        }
+        n1.v=pre_sum[i];
+        n1.idx=i;
+        push(n1);
+    }
+    free(stack);
+    free(pre_sum);
+    return out==12345678? -1:out;
 }
 #endif
 
