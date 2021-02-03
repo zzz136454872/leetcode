ccf99180182f984ad40d73d0b2fcc39c353e65f3
f4prime <zzz136454872@163.com>
2021-02-03 20:59:14 +0800

    add one

---
+++ countBalls.py
@@ -0,0 +1,18 @@
+from typing import *
+
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        log=[0 for i in range(100)]
+        def cnt(a):
+            return sum((int(i) for i in str(a)))
+        
+        for i in range(lowLimit,highLimit+1):
+            log[cnt(i)]+=1
+
+        return max(log)
+
+            
+sl=Solution()
+lowLimit = 5
+highLimit = 15
+print(sl.countBalls(lowLimit,highLimit))
