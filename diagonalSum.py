90b7e9497ead7b30ca88d0f522835307bda7597e
f4prime <zzz136454872@163.com>
2021-02-13 18:11:14 +0800

    add one

---
+++ diagonalSum.py
@@ -0,0 +1,16 @@
+from typing import *
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        out=0
+        n=len(mat)
+        for i in range(n):
+            out+=mat[i][i]
+            out+=mat[i][n-1-i]
+        if n%2!=0:
+            out-=mat[n//2][n//2]
+        return out
+
+mat = [[1,2,3], [4,5,6], [7,8,9]]
+sl=Solution()
+print(sl.diagonalSum(mat))
