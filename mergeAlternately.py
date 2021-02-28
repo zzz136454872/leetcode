f71422b46582d169d0c7c692c6dfdafc7079dc96
f4prime <zzz136454872@163.com>
2021-02-28 22:16:10 +0800

    add one

---
+++ mergeAlternately.py
@@ -0,0 +1,20 @@
+from typing import *
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        out=''
+        while word1 or word2:
+            if word1:
+                out+=word1[0]
+                word1=word1[1:]
+            if word2:
+                out+=word2[0]
+                word2=word2[1:]
+        return out
+
+sl=Solution()
+word1 = "ab"
+word2 = "pqrs"
+print(sl.mergeAlternately(word1,word2))
+# 输出："apbqcd"
+                
