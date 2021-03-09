c0f1d3b91ad28e1f9fce4adcb8a3a900f89046a7
f4prime <zzz136454872@163.com>
2021-03-09 10:17:15 +0800

    add one

---
+++ countOfAtoms.py
@@ -0,0 +1,111 @@
+from typing import *
+
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+
+        def count(nf):
+            # print(nf)
+            if nf.count('(')==0:
+                log={}
+                name=''
+                for i in range(len(nf)):
+                    l=nf[i]
+                    if l.isupper():
+                        if name!='':
+                            if name not in log.keys():
+                                log[name]=num_count
+                            else:
+                                log[name]+=num_count
+                        name=l
+                        num_count=1
+                    elif l.islower():
+                        name+=l
+                    elif l.isdigit():
+                        if nf[i-1].isdigit():
+                            num_count=num_count*10+int(l)
+                        else:
+                            num_count=int(l)
+                if name!='':
+                    if name not in log.keys():
+                        log[name]=num_count
+                    else:
+                        log[name]+=num_count
+                # print(log)
+                return log
+            i=0
+            log={}
+            level=0
+            name=''
+            while i<len(nf):
+                l=nf[i]
+                if level==0:
+                    if l.isupper():
+                        if name!='':
+                            if name not in log.keys():
+                                log[name]=num_count
+                            else:
+                                log[name]+=num_count
+                        name=l
+                        num_count=1
+                    elif l.islower():
+                        name+=l
+                    elif l.isdigit():
+                        if nf[i-1].isdigit():
+                            num_count=num_count*10+int(l)
+                        else:
+                            num_count=int(l)
+                    elif l=='(':
+                        if name!='':
+                            if name not in log.keys():
+                                log[name]=num_count
+                            else:
+                                log[name]+=num_count
+                        name=''
+                        level+=1
+                        start=i+1
+                    i+=1
+                else:
+                    num_count=1
+                    while level>0:
+                        l=nf[i]
+                        if l=='(':
+                            level+=1
+                        elif l==')':
+                            level-=1
+                            if level==0:
+                                nlog=count(nf[start:i])
+                        i+=1
+                    if i<len(nf) and nf[i].isdigit():
+                        num_count=int(nf[i])
+                        i+=1
+                    while i<len(nf) and nf[i].isdigit():
+                        num_count=num_count*10+int(nf[i])
+                        i+=1
+                    for k,v in nlog.items():
+                        if k not in log.keys():
+                            log[k]=num_count*v
+                        else:
+                            log[k]+=num_count*v
+
+            if name!='': 
+                if name not in log.keys():
+                    log[name]=num_count
+                else:
+                    log[name]+=num_count
+            # print(log)
+            return log
+
+        c=count(formula)
+        c=list(c.items())
+        c.sort()
+        out=''
+        for item in c:
+            out+=item[0]
+            if item[1]>1:
+                out+=str(item[1])
+        return out
+
+sl=Solution()
+formula="Mg(OH)2"
+print(sl.countOfAtoms(formula))
+    
--- countRestrictedPaths.py
+++ countRestrictedPaths.py
@@ -1,4 +1,5 @@
 from typing import *
+import heapq
 
 # 构建邻接表
 # 计算距离
@@ -16,30 +17,37 @@ class Solution:
         
         # 计算距离
         dist=[1234567890 for i in range(n+1)]
-        visited=[False for i in range(n+1)]
         dist[n]=0
-        visited[n]=True
-        for p in at[n]:
-            dist[p[0]]=p[1]
-        for point_count in range(n-1):
-            now_loc=0
-            now_dis=1234567890
-            for i in range(1,n):
-                if not visited[i] and now_dis>dist[i]:
-                    now_dis=dist[i]
-                    now_loc=i
-            visited[now_loc]=True
-            # print(now_loc,now_dis,visited,dist)
-            for p in at[now_loc]:
-                dist[p[0]]=min(dist[p[0]],now_dis+p[1])
+        heap=[n]
+        while len(heap)>0:
+            p=heapq.heappop(heap)
+            dist_now=dist[p]
+            for nextp,dist_next in at[p]:
+                if dist_next+dist_now<dist[nextp]:
+                    dist[nextp]=dist_next+dist_now
+                    heapq.heappush(heap,nextp)
 
-        print(dist)
+        # for p in at[n]:
+        #     dist[p[0]]=p[1]
+        # for point_count in range(n-1):
+        #     now_loc=0
+        #     now_dis=1234567890
+        #     for i in range(1,n):
+        #         if not visited[i] and now_dis>dist[i]:
+        #             now_dis=dist[i]
+        #             now_loc=i
+        #     visited[now_loc]=True
+        #     # print(now_loc,now_dis,visited,dist)
+        #     for p in at[now_loc]:
+        #         dist[p[0]]=min(dist[p[0]],now_dis+p[1])
+
+        # print(dist)
 
         # 排序
         order=[(dist[i],i) for i in range(1,len(dist))]
         order.sort()
 
-        print(order)
+        # print(order)
 
         # 遍历邻接表
         times=[0 for i in range(n+1)]
@@ -58,5 +66,7 @@ class Solution:
 sl=Solution()
 n = 7
 edges = [[1,3,1],[4,1,2],[7,3,4],[2,5,3],[5,6,1],[6,7,2],[7,5,3],[2,6,4]]
+n = 5
+edges = [[1,2,3],[1,3,3],[2,3,1],[1,4,2],[5,2,2],[3,5,1],[5,4,10]]
 print(sl.countRestrictedPaths(n,edges))
 
