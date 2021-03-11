52dd4c280718ed7d2de028a4e500b3d52395bae3
f4prime <zzz136454872@163.com>
2021-03-11 10:32:00 +0800

    add one

--- calculate.py
+++ calculate.py
@@ -1,47 +1,81 @@
 from typing import *
 
-class Solution:
+def get_priority(op):
+    if op=='+' or op=='-':
+        return 0
+    if op=='*' or op=='/':
+        return 1
+    if op=='(':
+        return -1
+    if op==')':
+        return 2
 
+def calc(num1,num2,op):
+    if op=='+':
+        return num1+num2
+    if op=='-':
+        return num1-num2
+    if op=='*':
+        return num1*num2
+    if op=='/':
+        return num1//num2
+
+class Solution:
     def calculate(self, s: str) -> int:
+        s=s.replace(' ','')
         l=[]
-        add=
         i=0
         while i<len(s):
-            if s[i]==' ':
-                i+=1
-                continue
             if s[i].isdigit():
                 num=int(s[i])
                 i+=1
-                while s[i].isdigit():
+                while i<len(s) and s[i].isdigit():
                     num=num*10+int(s[i])
                     i+=1
                 l.append(num)
             else:
+                if s[i]=='-' and (i==0 or not s[i-1].isdigit()):
+                    l.append(0)
                 l.append(s[i])
                 i+=1
-        return calc_sub(l)
+        i=0
+        num_stack=[]
+        op_stack=[]
 
-    def calc_sub(l):
-        if len(l)==1:
-            return l[0]
-        if s[-1]==')':
-            loc=s.index('(')
-            tmp=self.calc_sub(l[loc+1:len(l)-1])
-            if loc==0:
-                return tmp
+        while i<len(l):
+            # print(i,num_stack)
+            # print(i,op_stack)
+            if type(l[i])==int:
+                num_stack.append(l[i])
+            elif l[i]==')':
+                while op_stack[-1]!='(':
+                    op=op_stack.pop()
+                    num2=num_stack.pop()
+                    num1=num_stack.pop()
+                    num_stack.append(calc(num1,num2,op))
+                op_stack.pop()
+            elif l[i]=='(':
+                op_stack.append(l[i])
             else:
+                while len(op_stack)>0 and\
+                        get_priority(op_stack[-1])>=get_priority(l[i]):
+                    op=op_stack.pop()
+                    num2=num_stack.pop()
+                    num1=num_stack.pop()
+                    num_stack.append(calc(num1,num2,op))
+                op_stack.append(l[i])
+            i+=1
+        print('out')
+        while len(op_stack)>0:
+            print(op_stack)
+            print(num_stack)
+            op=op_stack.pop()
+            num2=num_stack.pop()
+            num1=num_stack.pop()
+            num_stack.append(calc(num1,num2,op))
+        print(num_stack)
+        return num_stack[0]
 
-        num=l[-1]
-        if l[-2] == '+':
-            return calc_sub(l[:-2])+num
-        if l[-2]=='-':
-            return calc_sub(l[:-2])-num
-
-                
-
-
-
-
-
-
+s="0-2147483647"
+sl=Solution()
+print(sl.calculate(s))
