396dcb4bdb523d762d1ff1e7fd1e07440da69491
f4prime <zzz136454872@163.com>
2021-03-25 09:35:37 +0800

    update pylist

--- pylist.py
+++ pylist.py
@@ -5,8 +5,19 @@ class ListNode:
     
     @classmethod
     def travel(self,head): # 从head开始遍历
+        if head==None:
+            print('empty linked list')
         while head!=None:
             print(head.val,end=' ')
             head=head.next
         print()
+
+    @classmethod
+    def fromList(self,nums):
+        virtual_head=ListNode(12345)
+        p=virtual_head
+        for num in nums:
+            p.next=ListNode(num)
+            p=p.next
+        return virtual_head.next
 
