38c2612e1a58d346b5e39ed8ef762a0002fcf9e4
f4prime <zzz136454872@163.com>
2021-03-29 09:29:39 +0800

    add one

---
+++ numDifferentIntegers.py
@@ -0,0 +1,19 @@
+from typing import *
+
+
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        word=list(word)
+        for i in range(len(word)):
+            if not word[i].isdigit():
+                word[i]=' '
+        word=''.join(word).split()
+        tmp=set()
+        for w in word:
+            tmp.add(int(w))
+        return len(tmp)
+
+sl=Solution()
+word = "a123bc34d8ef34"
+word = "a1b01c001"
+print(sl.numDifferentIntegers(word))
