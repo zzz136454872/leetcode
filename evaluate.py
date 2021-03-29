4c6eb5cf2b0c0b9c428121b3195075356d7d34c4
f4prime <zzz136454872@163.com>
2021-03-29 09:29:10 +0800

    add one

---
+++ evaluate.py
@@ -0,0 +1,28 @@
+from typing import *
+
+class Solution:
+    def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
+        knowledge=dict(knowledge)
+
+        out=[]
+        i=0
+        while i<len(s):
+            if s[i]=='(':
+                j=i+1
+                while s[j]!=')':
+                    j+=1
+                key=s[i+1:j]
+                if key in knowledge:
+                    out.append(knowledge[key])
+                else:
+                    out.append('?')
+                i=j
+            else:
+                out.append(s[i])
+            i+=1
+        return ''.join(out)
+
+sl=Solution()
+s = "(name)is(age)yearsold"
+knowledge = [["name","bob"],["age","two"]]
+print(sl.evaluate(s,knowledge))
