c3454016c36d18cf8bccce21dbe8d79cc98432ef
f4prime <zzz136454872@163.com>
2021-03-31 09:34:17 +0800

    add one

--- singleNumber.py
+++ singleNumber.py
@@ -7,3 +7,21 @@ class Solution:
             out=out^num
         return out
 
+#I. 数组中数字出现的次数
+
+from functools import reduce
+class Solution:
+    def singleNumbers(self, nums: List[int]) -> List[int]:
+        ab=reduce(lambda a,b:a^b,nums)
+        last=ab&(-ab)
+        a=0
+        for num in nums:
+            if num&last:
+                a^=num
+        return [a,ab^a]
+
+sl=Solution()
+nums = [4,1,4,6]
+print(sl.singleNumbers(nums))
+        
+        
