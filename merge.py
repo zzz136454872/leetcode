95cf0921234ddc6ab120b6d5ad807b94708fc03f
f4prime <zzz136454872@163.com>
2021-04-05 09:57:39 +0800

    add one

--- merge.py
+++ merge.py
@@ -1,7 +1,6 @@
 from typing import *
 
-
-class Solution:
+class Solution1:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x:x[0])
         i=0
@@ -14,11 +13,44 @@ class Solution:
             else:
                 i+=1
         return intervals
-                
-inp=[[1,3],[2,6],[8,10],[15,18]]
-sl=Solution()
-print(sl.merge(inp))
+
+# inp=[[1,3],[2,6],[8,10],[15,18]]
+# sl=Solution()
+# print(sl.merge(inp))
             
+# 合并两个有序数组
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums12=nums1[:m]
+        p1=0
+        p2=0
+        while p1<m or p2<n:
+            if p2==n:
+                nums1[p1+p2]=nums12[p1]
+                p1+=1
+                continue
+            if p1==m:
+                nums1[p1+p2]=nums2[p2]
+                p2+=1
+                continue
+            if nums12[p1]<nums2[p2]:
+                nums1[p1+p2]=nums12[p1]
+                p1+=1
+            else:
+                nums1[p1+p2]=nums2[p2]
+                p2+=1
+
+sl=Solution()
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+sl.merge(nums1,m,nums2,n)
+print(nums1)
+            
+            
+
+
             
         
 
