558fe2b5eb93f7cc11fb49e0e0ffed9da81506d5
f4prime <zzz136454872@163.com>
2021-04-25 12:02:45 +0800

    add one

---
+++ increasingBST.py
@@ -0,0 +1,35 @@
+from typing import *
+
+from pytree import TreeNode
+
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        out = None
+        pre = None
+
+        def midorder(root):
+            if root is None:
+                return
+            midorder(root.left)
+            nonlocal pre
+            root.left=None
+
+            if pre is not None:
+                pre.right = root
+            else:
+                nonlocal out
+                out = root
+            pre = root
+            midorder(root.right)
+
+        midorder(root)
+
+        return out
+
+
+inp='[2,1,4,null,null,3]'
+root = TreeNode.fromStrList(inp)
+sl = Solution()
+out = sl.increasingBST(root)
+TreeNode.travel(out)
--- pytree.py
+++ pytree.py
@@ -1,53 +1,70 @@
-# Definition for a binary tree node.
 class TreeNode:
+    # Definition for a binary tree node.
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
-    def travel(self,order='pre'):
+    def travel(self, order='pre'):
         # order : pre,mid,post
-        if order!='pre' and order!='mid' and order!='post':
-            print('order error: ',order)
+
+        if order != 'pre' and order != 'mid' and order != 'post':
+            print('order error: ', order)
         self.travel_sub(order)
         print()
 
-    def travel_sub(self,order):
-        if order=='pre':
+    def travel_sub(self, order):
+        if order == 'pre':
             print(self.val, end=' ')
-        if self.left!=None:
+
+        if self.left is not None:
             self.left.travel_sub(order)
-        if order=='mid':
+
+        if order == 'mid':
             print(self.val, end=' ')
-        if self.right!=None:
+
+        if self.right is not None:
             self.right.travel_sub(order)
-        if order=='post':
+
+        if order == 'post':
             print(self.val, end=' ')
 
     @classmethod
-    def fromStrList(self,s):
-        null=None
-        data=eval(s)
-        if len(data)==1:
+    def fromStrList(self, s):
+        null = None
+        data = eval(s)
+
+        if len(data) == 1:
             return None
-        root=TreeNode(data[0])
-        queue=[root]
+        root = TreeNode(data[0])
+        queue = [root]
         data.pop(0)
-        while len(data)>0:
-            node=queue.pop(0)
-            left=data.pop(0)
-            right=data.pop(0)
-            if left!=None:
-                node.left=TreeNode(left)
+
+        while len(data) > 0:
+            node = queue.pop(0)
+
+            if len(data) > 0:
+                left = data.pop(0)
+            else:
+                left = None
+
+            if len(data) > 0:
+                right = data.pop(0)
+            else:
+                right = None
+
+            if left is not None:
+                node.left = TreeNode(left)
                 queue.append(node.left)
-            if right!=None:
-                node.right=TreeNode(right)
+
+            if right is not None:
+                node.right = TreeNode(right)
                 queue.append(node.right)
+
         return root
-    
 
-if __name__=='__main__':
-    inp='[7, 3, 15, null, null, 9, 20]'
-    root=TreeNode.fromStrList(inp)
+
+if __name__ == '__main__':
+    inp = '[7, 3, 15, null, null, 9, 20]'
+    root = TreeNode.fromStrList(inp)
     root.travel("mid")
-
