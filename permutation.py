770b1fe3435213f592aee55e304a3d6225601b5d
f4prime <zzz136454872@163.com>
2021-06-23 09:38:44 +0800

    update one

--- permutation.py
+++ permutation.py
@@ -3,36 +3,37 @@ from typing import List
 
 class Solution:
     def permutation(self, s: str) -> List[str]:
-        lists=[ord(letter) for letter in s]
+        lists = [ord(letter) for letter in s]
         lists.sort()
-        n=len(lists)
-        out=[]
-        
+        n = len(lists)
+        out = []
+
         while True:
             out.append(''.join([chr(i) for i in lists]))
-            i=n-1
-            while i>0 and lists[i]<=lists[i-1]:
-                i-=1
-            if i==0:
+            i = n - 1
+
+            while i > 0 and lists[i] <= lists[i - 1]:
+                i -= 1
+
+            if i == 0:
                 break
-            i-=1
-            j=n-1
-            while lists[j]<=lists[i]:
-                j-=1
-            lists[i],lists[j]=lists[j],lists[i]
-            i+=1
-            j=n-1
-            while i<j:
-                lists[i],lists[j]=lists[j],lists[i]
-                i+=1
-                j-=1
+            i -= 1
+            j = n - 1
+
+            while lists[j] <= lists[i]:
+                j -= 1
+            lists[i], lists[j] = lists[j], lists[i]
+            i += 1
+            j = n - 1
+
+            while i < j:
+                lists[i], lists[j] = lists[j], lists[i]
+                i += 1
+                j -= 1
 
         return out
 
-sl=Solution()
+
+sl = Solution()
 s = "bbc"
 print(sl.permutation(s))
-
-
-
-                
