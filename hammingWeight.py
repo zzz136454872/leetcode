ed08d1decde1f643fea04ec1e939f1f6256bdd2e
f4prime <zzz136454872@163.com>
2021-06-23 16:43:52 +0800

    update one

--- hammingWeight.py
+++ hammingWeight.py
@@ -1,14 +1,18 @@
 from typing import *
 
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        out=0
-        while n!=0:
-            n=n&(n-1)
+        out = 0
+
+        while n != 0:
+            n = n & (n - 1)
             # print(n)
-            out+=1
+            out += 1
+
         return out
 
-sl=Solution()
-inp=int('1111',2)
+
+sl = Solution()
+inp = int('1111', 2)
 print(sl.hammingWeight(inp))
