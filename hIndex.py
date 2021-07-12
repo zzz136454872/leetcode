a06c604542ea9f74c9b73f7dd3c672a53718bcb2
f4prime <zzz136454872@163.com>
2021-07-12 16:05:29 +0800

    add one

--- hIndex.py
+++ hIndex.py
@@ -1,7 +1,8 @@
 from typing import List
 
 
-class Solution:
+# H指数
+class Solution1:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
 
@@ -12,6 +13,25 @@ class Solution:
         return len(citations)
 
 
+# H指数II
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        left = 0
+        n = len(citations)
+        right = n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if n - mid > citations[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return n - left
+
+
 sl = Solution()
-citations = [100,100,5]
+citations = [100, 100, 5]
+citations = [0, 1, 3, 5, 6]
 print(sl.hIndex(citations))
