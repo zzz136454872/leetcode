0a79b44b646bcd24dbc84e98844a4d99383018ae
f4prime <zzz136454872@163.com>
2021-07-16 11:26:44 +0800

    add one

--- search.py
+++ search.py
@@ -1,11 +1,18 @@
-from typing import *
+from typing import List
 
-class Solution:
+
+class Solution1:
     def search(self, nums: List[int], target: int) -> bool:
         return target in nums
 
-sl=Solution()
-nums = [1,0,1,1,1]
-target = 0
-print(sl.search(nums,target))
 
+# I. 在排序数组中查找数字 I
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return nums.count(target)
+
+
+sl = Solution()
+nums = [1, 0, 1, 1, 1]
+target = 0
+print(sl.search(nums, target))
