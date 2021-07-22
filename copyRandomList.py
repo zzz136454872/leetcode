2cbe655953727060fa50661024f6085512193e3b
f4prime <zzz136454872@163.com>
2021-07-22 09:24:42 +0800

    add one

--- copyRandomList.py
+++ copyRandomList.py
@@ -1,5 +1,5 @@
-from typing import *
-from copy import deepcopy
+import copy
+
 
 # Definition for a Node.
 class Node:
@@ -8,8 +8,38 @@ class Node:
         self.next = next
         self.random = random
 
-class Solution:
+
+# 复制带随机指针的链表
+class Solution1:
     def copyRandomList(self, head: 'Node') -> 'Node':
         return copy.deepcopy(head)
 
 
+# 上一个题的另一种解法
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None:
+            return None
+        p = head
+        q = Node(p.val)
+        outHead = q
+        mem = {p: q}
+
+        while p is not None:
+            p = p.next
+
+            if p is None:
+                break
+            q.next = Node(p.val)
+            q = q.next
+            mem[p] = q
+        mem[None] = None
+        p = head
+        q = outHead
+
+        while p is not None:
+            q.random = mem[p.random]
+            p = p.next
+            q = q.next
+
+        return outHead
