829141f8485022e7c177e468a23cb9eb91bb98bc
f4prime <zzz136454872@163.com>
2021-08-13 09:16:32 +0800

    add one

--- countDigitOne.py
+++ countDigitOne.py
@@ -1,20 +1,18 @@
-from typing import *
-
 class Solution:
     def countDigitOne(self, n: int) -> int:
-        base=1
-        out=0
-        while n>0:
-            if n%10>0:
-                out+=base
-            n=n//10
-            out+=n*base
-            base=base*10
+        base = 1
+        out = 0
+
+        while n >= base:
+            out += n // (base * 10) * base + min(
+                max(n % (base * 10) - base + 1, 0), base)
+            base *= 10
+
         return out
 
-sl=Solution()
-n=10
-print(sl.countDigitOne(n))
 
-            
-                
+sl = Solution()
+n = 10
+n = 13
+n = 1
+print(sl.countDigitOne(n))
