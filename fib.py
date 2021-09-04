1f973efeac66dcadc6c68b0ba90e4d314593f1b1
f4prime <zzz136454872@163.com>
2021-09-04 10:13:35 +0800

    add one

--- fib.py
+++ fib.py
@@ -1,12 +1,37 @@
 from typing import *
 
+
+# 斐波那契数列
 class Solution:
     def fib(self, n: int) -> int:
-        if n<=1:
+        if n <= 1:
             return n
-        log=[0 for i in range(n+1)]
-        log[0]=0
-        log[1]=1
-        for i in range(2,n+1):
-            log[i]=log[i-1]+log[i-2]
+        log = [0 for i in range(n + 1)]
+        log[0] = 0
+        log[1] = 1
+
+        for i in range(2, n + 1):
+            log[i] = log[i - 1] + log[i - 2]
+
         return log[n]
+
+
+# 斐波那契数列 with a mod
+class Solution:
+    def fib(self, n: int) -> int:
+        mod = 10**9 + 7
+
+        if n <= 1:
+            return n
+        now = 1
+        pre = 0
+
+        for i in range(n - 1):
+            now, pre = (now + pre) % mod, now
+
+        return now
+
+
+n = 2
+n = 5
+print(Solution().fib(n))
