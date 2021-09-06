04ffa37ad9b775ccf8e63a27f19b93740dc85c3b
f4prime <zzz136454872@163.com>
2021-09-06 09:31:08 +0800

    add one

--- search.py
+++ search.py
@@ -12,6 +12,25 @@ class Solution:
         return nums.count(target)
 
 
+# 二分查找
+class Solution1:
+    def search(self, nums: List[int], target: int) -> bool:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                return mid
+
+        return -1
+
+
 sl = Solution()
 nums = [1, 0, 1, 1, 1]
 target = 0
