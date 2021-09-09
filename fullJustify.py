c5b0a1adde76c72e4aa2c93c31a10e28dc4e97a5
f4prime <zzz136454872@163.com>
2021-09-09 11:44:41 +0800

    add one

--- fullJustify.py
+++ fullJustify.py
@@ -1,54 +1,66 @@
 from typing import *
 
+
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        tmp=[]
-        tmplen=0
-        out=[]
+        tmp = []
+        tmplen = 0
+        out = []
+
         for word in words:
-            if len(tmp)==0:
+            if len(tmp) == 0:
                 tmp.append(word)
-                tmplen+=len(word)
+                tmplen += len(word)
+
                 continue
-            if tmplen+len(word)+1<=maxWidth:
+
+            if tmplen + len(word) + 1 <= maxWidth:
                 tmp.append(word)
-                tmplen+=len(word)+1
+                tmplen += len(word) + 1
+
                 continue
-            if len(tmp)>1:
-                rest=maxWidth-tmplen
-                avg=rest//(len(tmp)-1)
-                buf=[1+avg for i in range(len(tmp)-1)]
-                r=rest-(len(tmp)-1)*avg
+
+            if len(tmp) > 1:
+                rest = maxWidth - tmplen
+                avg = rest // (len(tmp) - 1)
+                buf = [1 + avg for i in range(len(tmp) - 1)]
+                r = rest - (len(tmp) - 1) * avg
+
                 for i in range(r):
-                    buf[i]+=1
-                tmpout=''
-                for i in range(len(tmp)-1):
-                    tmpout+=tmp[i]+buf[i]*' '
-                tmpout+=tmp[-1]
+                    buf[i] += 1
+                tmpout = ''
+
+                for i in range(len(tmp) - 1):
+                    tmpout += tmp[i] + buf[i] * ' '
+                tmpout += tmp[-1]
                 out.append(tmpout)
             else:
-                out.append(tmp[0]+(maxWidth-len(tmp[0]))*' ')
-            tmp=[word]
-            tmplen=len(word)
-        if len(tmp)>0:
-            tmpout=''
+                out.append(tmp[0] + (maxWidth - len(tmp[0])) * ' ')
+            tmp = [word]
+            tmplen = len(word)
+
+        if len(tmp) > 0:
+            tmpout = ''
+
             for word in tmp:
-                if len(tmpout)>0:
-                    tmpout+=' '
-                tmpout+=word
-            tmpout+=(maxWidth-len(tmpout))*' '
+                if len(tmpout) > 0:
+                    tmpout += ' '
+                tmpout += word
+            tmpout += (maxWidth - len(tmpout)) * ' '
             out.append(tmpout)
+
         return out
 
+
 words = ["This", "is", "an", "example", "of", "text", "justification."]
-words = ["What","must","be","acknowledgment","shall","be"]
-words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"]
+words = ["What", "must", "be", "acknowledgment", "shall", "be"]
+words = [
+    "Science", "is", "what", "we", "understand", "well", "enough", "to",
+    "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we",
+    "do"
+]
 
 maxWidth = 16
 maxWidth = 20
-sl=Solution()
-print(sl.fullJustify(words,maxWidth))
-                
-
-
-
+sl = Solution()
+print(sl.fullJustify(words, maxWidth))
--- maxScore.py
+++ maxScore.py
@@ -1,50 +1,86 @@
 from typing import *
 
+
+# 不知道是哪个
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        n=len(cardPoints)
-        log1=[0 for i in range(n+1)]
-        log2=[0 for i in range(n+1)]
-        tmp=0
+        n = len(cardPoints)
+        log1 = [0 for i in range(n + 1)]
+        log2 = [0 for i in range(n + 1)]
+        tmp = 0
+
         for i in range(n):
-            tmp+=cardPoints[i]
-            log1[i+1]=tmp
-        tmp=0
-        for i in range(n-1,-1,-1):
-            tmp+=cardPoints[i]
-            log2[i]=tmp
-        out=0
+            tmp += cardPoints[i]
+            log1[i + 1] = tmp
+        tmp = 0
+
+        for i in range(n - 1, -1, -1):
+            tmp += cardPoints[i]
+            log2[i] = tmp
+        out = 0
         # print(log1)
         # print(log2)
-        for i in range(k+1):
-            out=max(out,log1[i]+log2[n-k+i])
+
+        for i in range(k + 1):
+            out = max(out, log1[i] + log2[n - k + i])
             # print(i,out)
+
         return out
 
+
 # N 次操作后的最大分数和
-def gcd(n1,n2):
-    while n2!=0:
-        n1,n2=n2,n1%n2
+def gcd(n1, n2):
+    while n2 != 0:
+        n1, n2 = n2, n1 % n2
+
     return n1
+
 
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
-        g={n1:{n2:gcd(n1,n2) for n2 in nums }for n1 in nums}
-        def calc(level,nums):
-            if len(nums)==0:
+        g = {n1: {n2: gcd(n1, n2) for n2 in nums} for n1 in nums}
+
+        def calc(level, nums):
+            if len(nums) == 0:
                 return 0
-            max_num=0
-            for i in range(len(nums)-1):
-                for j in range(i+1,len(nums)):
-                    new_num=nums.copy()
+            max_num = 0
+
+            for i in range(len(nums) - 1):
+                for j in range(i + 1, len(nums)):
+                    new_num = nums.copy()
                     del new_num[j]
                     del new_num[i]
-                    now=g[nums[i]][nums[j]]*level+calc(level+1,new_num)
-                    max_num=max(max_num,now)
+                    now = g[nums[i]][nums[j]] * level + calc(
+                        level + 1, new_num)
+                    max_num = max(max_num, now)
+
             return max_num
-        return calc(1,nums)
 
-sl=Solution()
-nums = [1,2,3,4,5,6,1,2,3,4]
-print(sl.maxScore(nums))
+        return calc(1, nums)
 
+
+#  分割字符串的最大得分
+class Solution:
+    def maxScore(self, s: str) -> int:
+        now = s[1:].count('1')
+
+        if s[0] == '0':
+            now += 1
+        out = now
+
+        for letter in s[1:len(s) - 1]:
+            if letter == '0':
+                now += 1
+            else:
+                now -= 1
+            out = max(now, out)
+
+        return out
+
+
+sl = Solution()
+nums = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4]
+s = "011101"
+s = "00111"
+# s = "111"
+print(sl.maxScore(s))
