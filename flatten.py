2fb8e245408d2dff34600234b6a83b048103838c
f4prime <zzz136454872@163.com>
2021-09-24 09:13:19 +0800

    add one

--- flatten.py
+++ flatten.py
@@ -1,5 +1,6 @@
 from typing import *
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,24 +8,69 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
+
+# 不知道是什么
+class Solution1:
     def flatten(self, root: TreeNode) -> None:
-        if root==None:
+        if root == None:
             return
         self.sub(root)
 
-    def sub(self,root):
-        if root.left!=None:
+    def sub(self, root):
+        if root.left != None:
             self.sub(root.left)
-        if root.right!=None:
+
+        if root.right != None:
             self.sub(root.right)
-        if root.left!=None:
-            tmp=root.right
-            root.right=root.left
-            root.left=None
-            p=root
-            while p.right!=None:
-                p=p.right
-            p.right=tmp
-       
-        
+
+        if root.left != None:
+            tmp = root.right
+            root.right = root.left
+            root.left = None
+            p = root
+
+            while p.right != None:
+                p = p.right
+            p.right = tmp
+
+
+# 扁平化多级双向链表
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        def flattenSub(head):
+            if head is None:
+                return (None, None)
+            p = head
+            prev = p
+
+            while p is not None:
+                if p.child is not None:
+                    q = p.next
+                    tmp = flattenSub(p.child)
+                    p.child = None
+                    p.next = tmp[0]
+                    tmp[0].prev = p
+                    tmp[1].next = q
+
+                    if q is not None:
+                        q.prev = tmp[1]
+                    prev = tmp[1]
+                    p = q
+                else:
+                    prev = p
+                    p = p.next
+
+            return (head, prev)
+
+        return flattenSub(head)[0]
