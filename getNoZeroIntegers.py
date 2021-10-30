628781f6b565cd36e6181c2119e32f5cac5efcc4
f4prime <zzz136454872@163.com>
2021-10-30 10:59:30 +0800

    add one

---
+++ getNoZeroIntegers.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for i in range(1, n):
+            if '0' not in str(i) and '0' not in str(n - i):
+                return [i, n - i]
+
+        return [0, 0]
--- singleNumber.py
+++ singleNumber.py
@@ -1,27 +1,34 @@
 from typing import *
 
+
 class Solution:
     def singleNumbers(self, nums: List[int]) -> List[int]:
-        out=0
+        out = 0
+
         for num in nums:
-            out=out^num
+            out = out ^ num
+
         return out
+
 
 #I. 数组中数字出现的次数
 
 from functools import reduce
+
+
 class Solution:
     def singleNumbers(self, nums: List[int]) -> List[int]:
-        ab=reduce(lambda a,b:a^b,nums)
-        last=ab&(-ab)
-        a=0
-        for num in nums:
-            if num&last:
-                a^=num
-        return [a,ab^a]
+        ab = reduce(lambda a, b: a ^ b, nums)
+        last = ab & (-ab)
+        a = 0
 
-sl=Solution()
-nums = [4,1,4,6]
+        for num in nums:
+            if num & last:
+                a ^= num
+
+        return [a, ab ^ a]
+
+
+sl = Solution()
+nums = [4, 1, 4, 6]
 print(sl.singleNumbers(nums))
-        
-        
