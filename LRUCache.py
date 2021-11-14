90eda47ef4932f6b0e0abd26aeb9a16ad0f12477
f4prime <zzz136454872@163.com>
2021-11-14 09:59:55 +0800

    add one

--- LRUCache.py
+++ LRUCache.py
@@ -1,43 +1,107 @@
-class LRUCache(object):
-
+# TLE
+class LRUCache1(object):
     def __init__(self, capacity):
-        self.mem=list()
-        self.cap=capacity
+        self.mem = list()
+        self.cap = capacity
 
     def get(self, key):
         for i in range(len(self.mem)):
-            print(i,self.mem[i])
+            print(i, self.mem[i])
+
             if self.mem[i][0] == key:
-                tmp=self.mem[i]
-                print("tmp",tmp)
+                tmp = self.mem[i]
+                print("tmp", tmp)
                 del self.mem[i]
-                self.mem.insert(0,tmp)
+                self.mem.insert(0, tmp)
                 print(self.mem)
+
                 return tmp[1]
         print(self.mem)
+
         return -1
 
     def put(self, key, value):
         for i in range(len(self.mem)):
-            if self.mem[i][0]==key:
-                tmp=self.mem[i]
+            if self.mem[i][0] == key:
+                tmp = self.mem[i]
                 del self.mem[i]
-                tmp[1]=value
-                self.mem.insert(0,tmp)
+                tmp[1] = value
+                self.mem.insert(0, tmp)
                 print(self.mem)
-                return 
+
+                return
+
         if self.cap == len(self.mem):
             del self.mem[-1]
-        self.mem.insert(0,[key,value])
+        self.mem.insert(0, [key, value])
         print(self.mem)
 
-cache=LRUCache(2)
+
+class ListNode:
+    def __init__(self, key, val):
+        self.val = val
+        self.key = key
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.mem = dict()
+        self.cap = capacity
+        self.front = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.front.next = self.tail
+        self.tail.prev = self.front
+
+    def get(self, key: int) -> int:
+        if key not in self.mem:
+            return -1
+        node = self.mem[key]
+        self.remove(node)
+        self.insertFront(node)
+
+        return node.val
+
+    def insertFront(self, newNode):
+        newNode.next = self.front.next
+        newNode.prev = self.front
+        newNode.next.prev = newNode
+        self.front.next = newNode
+
+    def remove(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.mem:
+            self.mem[key].val = value
+            newNode = self.mem[key]
+            self.remove(newNode)
+        else:
+            newNode = ListNode(key, value)
+            self.mem[key] = newNode
+        self.insertFront(newNode)
+        # print('mem len',len(self.mem))
+
+        if len(self.mem) > self.cap:
+            # print('del')
+            del self.mem[self.tail.prev.key]
+            self.remove(self.tail.prev)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+cache = LRUCache(2)
 print(cache.put(1, 1))
 print(cache.put(2, 2))
 print(cache.get(1))
 print(cache.put(3, 3))
-print(cache.get(2)) 
+print(cache.get(2))
 print(cache.put(4, 4))
-print(cache.get(1)) 
+print(cache.get(1))
 print(cache.get(3))
 print(cache.get(4))
--- MapSum.py
+++ MapSum.py
@@ -1,46 +1,58 @@
 from typing import *
 
+
 class Node:
-    def __init__(self,value):
-        self.son=[None for i in range(26)]
-        self.value=value
-    
+    def __init__(self, value):
+        self.son = [None for i in range(26)]
+        self.value = value
+
     def print(self):
-        print(self.value,':',end=' ')
+        print(self.value, ':', end=' ')
+
         for i in range(len(self.son)):
-            print(chr(ord('a')+i),self.son[i],end='')
+            print(chr(ord('a') + i), self.son[i], end='')
+
 
 class MapSum:
     def __init__(self):
-        self.root=Node(0)
+        self.root = Node(0)
 
     def insert(self, key: str, val: int) -> None:
-        root=self.root
+        root = self.root
+
         for letter in key:
-            num=ord(letter)-ord('a')
-            if root.son[num]==None:
-                root.son[num]=Node(0)
-            root=root.son[num]
-        root.value=val
+            num = ord(letter) - ord('a')
+
+            if root.son[num] == None:
+                root.son[num] = Node(0)
+            root = root.son[num]
+        root.value = val
         #print(root.value)
 
     def sum(self, prefix: str) -> int:
-        root=self.root
+        root = self.root
+
         for letter in prefix:
-            num=ord(letter)-ord('a')
-            if root.son[num]==None:
+            num = ord(letter) - ord('a')
+
+            if root.son[num] == None:
                 print('false')
+
                 return 0
-            root=root.son[num]
+            root = root.son[num]
+
         return self.son_sum(root)
 
-    def son_sum(self,root):
-        if root==None:
+    def son_sum(self, root):
+        if root == None:
             return 0
-        out=root.value
+        out = root.value
+
         for i in range(26):
-            out+=self.son_sum(root.son[i])
+            out += self.son_sum(root.son[i])
+
         return out
+
 
 obj = MapSum()
 obj.insert("apple", 3)
