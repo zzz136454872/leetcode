0faa9f4bdb8f5998876267eecc71e189c72c5d45
f4prime <zzz136454872@163.com>
2021-12-28 09:59:21 +0800

    update pytrie

--- pyTrie.py
+++ pyTrie.py
@@ -1,4 +1,19 @@
+def l2n(a):
+    return ord(a) - ord('a')
+
+
 class Trie:
     def __init__(self):
         self.has = False
         self.sons = [None for i in range(26)]
+
+    def add(self, word):
+        p = self
+
+        for letter in word:
+            idx = l2n(letter)
+
+            if p.sons[idx] is None:
+                p.sons[idx] = Trie()
+            p = p.sons[idx]
+        p.has = True
