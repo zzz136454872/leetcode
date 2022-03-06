e7ef172e885abc8da0c63617bfdbdcb062948712
f4prime <zzz136454872@163.com>
2022-03-06 17:43:49 +0800

    add one

--- subArrayRanges.py
+++ subArrayRanges.py
@@ -3,8 +3,20 @@ from typing import List
 
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        for i in range(len(nums)-1):
-            minStack=[nums[i]]
-            maxStack=[nums[i]]
-            for j in range(i+1,len(nums)):
+        out = 0
 
+        for i in range(len(nums) - 1):
+            m1 = nums[i]
+            m2 = nums[i]
+
+            for j in range(i + 1, len(nums)):
+                m1 = max(m1, nums[j])
+                m2 = min(m2, nums[j])
+                out += m1 - m2
+
+        return out
+
+
+nums = [1, 2, 3]
+nums = [1, 3, 3]
+print(Solution().subArrayRanges(nums))
