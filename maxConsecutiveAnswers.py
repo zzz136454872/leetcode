4914dea1e612a4da57824b76637456b96dbece12
f4prime <zzz136454872@163.com>
2022-03-29 15:35:48 +0800

    add one

--- maxConsecutiveAnswers.py
+++ maxConsecutiveAnswers.py
@@ -3,13 +3,31 @@ from typing import List
 
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        l=0
-        r=0
-        t_count=0
-        f_count=0
-        out=0
-        for l in range(len(answerKey)):
-            while t_count<k and f_count<k and r<len(answerKey):
-                if answerKey[r]>
-                r+=1
-                    
+        def long(letter):
+            out = 0
+            r = 0
+            count = 0
+
+            for l in range(len(answerKey)):
+                while r < len(answerKey) and (count < k or count == k
+                                              and answerKey[r] == letter):
+                    if answerKey[r] != letter:
+                        count += 1
+                    r += 1
+                out = max(out, r - l)
+
+                if answerKey[l] != letter:
+                    count -= 1
+
+            return out
+
+        return max(long('F'), long('T'))
+
+
+answerKey = "TTFF"
+k = 2
+answerKey = "TFFT"
+k = 1
+answerKey = "TTFTTFTT"
+k = 1
+print(Solution().maxConsecutiveAnswers(answerKey, k))
