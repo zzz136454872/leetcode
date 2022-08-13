916ed20468d4d92dd9e34725264bb460c4a266d9
f4prime <zzz136454872@163.com>
2022-08-13 11:05:16 +0800

    add one

---
+++ maxChunksToSorted.py
@@ -0,0 +1,39 @@
+from typing import List
+
+# 768. 最多能完成排序的块 II
+
+
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        def same(a, b):
+            try:
+                for k in a:
+                    if a[k] != b[k]:
+                        return False
+
+                return True
+            except:
+                return False
+
+        count = 0
+        arr2 = arr.copy()
+        arr2.sort()
+
+        a = {}
+        b = {}
+
+        for i in range(len(arr)):
+            a[arr[i]] = a.get(arr[i], 0) + 1
+            b[arr2[i]] = b.get(arr2[i], 0) + 1
+
+            if same(a, b):
+                a = {}
+                b = {}
+                count += 1
+
+        return count
+
+
+arr = [5, 4, 3, 2, 1]
+arr = [2, 1, 3, 4, 4]
+print(Solution().maxChunksToSorted(arr))
