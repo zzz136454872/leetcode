44710f9d05e2c6384cce16396073b880d25dc598
f4prime <zzz136454872@163.com>
2023-03-04 11:41:44 +0800

    add one

--- countTriplets.py
+++ countTriplets.py
@@ -1,19 +1,45 @@
-from typing import *
+from collections import Counter
+from typing import List
 
-class Solution:
+
+# 不知道是哪个
+class Solution1:
     def countTriplets(self, arr: List[int]) -> int:
-        out=0
-        n=len(arr)
-        for i in range(n-1):
-            tmp=arr[i]
-            for j in range(i+1,n):
-                tmp^=arr[j]
-                if tmp==0:
-                    out+=j-i
+        out = 0
+        n = len(arr)
+
+        for i in range(n - 1):
+            tmp = arr[i]
+
+            for j in range(i + 1, n):
+                tmp ^= arr[j]
+
+                if tmp == 0:
+                    out += j - i
+
         return out
 
-sl=Solution()
-arr = [7,11,12,9,5,2,7,17,22]
-print(sl.countTriplets(arr))
 
-                
+# sl=Solution()
+# arr = [7,11,12,9,5,2,7,17,22]
+# print(sl.countTriplets(arr))
+
+
+class Solution:
+    def countTriplets(self, nums: List[int]) -> int:
+        mem = Counter([x & y for x in nums for y in nums])
+        res = 0
+
+        for k in mem:
+            t = mem[k]
+
+            for x in nums:
+                if x & k == 0:
+                    res += t
+
+        return res
+
+
+nums = [2, 1, 3]
+
+print(Solution().countTriplets(nums))
