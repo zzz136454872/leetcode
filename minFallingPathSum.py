2be68f4fcecb93ff5b805b4ed7fb058bee74a4ad
f4prime <zzz136454872@163.com>
2023-08-10 10:25:59 +0800

    add one

--- minFallingPathSum.py
+++ minFallingPathSum.py
@@ -1,7 +1,8 @@
 from typing import List
 
 
-class Solution:
+# 不知道是哪个
+class Solution1:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         mem = matrix[0]
@@ -20,6 +21,30 @@ class Solution:
         return min(mem)
 
 
-matrix = [[2, 1, 3], [6, 5, 4], [7, 8, 9]]
-matrix = [[-19, 57], [-40, -5]]
-print(Solution().minFallingPathSum(matrix))
+# matrix = [[2, 1, 3], [6, 5, 4], [7, 8, 9]]
+# matrix = [[-19, 57], [-40, -5]]
+# print(Solution().minFallingPathSum(matrix))
+
+
+# 1289. 下降路径最小和 II
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        mem = grid[0]
+
+        for i in range(1, n):
+            newMem = grid[i].copy()
+            mem = sorted([(mem[j], j) for j in range(n)])
+
+            for j in range(n):
+                if j != mem[0][1]:
+                    newMem[j] += mem[0][0]
+                else:
+                    newMem[j] += mem[1][0]
+            mem = newMem
+
+        return min(mem)
+
+
+grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(Solution().minFallingPathSum(grid))
