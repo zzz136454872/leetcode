8d2b9789f5222bf1865b0225fad563171fbb43b7
f4prime <zzz136454872@163.com>
2023-08-11 10:53:53 +0800

    add one

--- diagonalSum.py
+++ diagonalSum.py
@@ -1,16 +1,21 @@
-from typing import *
+from typing import List
+
 
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        out=0
-        n=len(mat)
+        out = 0
+        n = len(mat)
+
         for i in range(n):
-            out+=mat[i][i]
-            out+=mat[i][n-1-i]
-        if n%2!=0:
-            out-=mat[n//2][n//2]
+            out += mat[i][i]
+            out += mat[i][n - 1 - i]
+
+        if n % 2 != 0:
+            out -= mat[n // 2][n // 2]
+
         return out
 
-mat = [[1,2,3], [4,5,6], [7,8,9]]
-sl=Solution()
+
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+sl = Solution()
 print(sl.diagonalSum(mat))
---
+++ findPeakGrid.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        k = 1234567
+        m = 112347
+        t = 1
+        i = 0
+        j = 0
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while True:
+            t = (t * k) % m
+            idx = t % 4
+            flag = True
+
+            for n in range(4):
+                ni = i + dirs[n][0]
+                nj = j + dirs[n][1]
+
+                if ni < 0 or ni >= len(mat) or nj < 0 or nj >= len(mat[0]):
+                    continue
+
+                if mat[ni][nj] > mat[i][j]:
+                    print('from', i, j, 'to', ni, nj)
+                    flag = False
+                    i = ni
+                    j = nj
+
+                    break
+
+            if flag:
+                return (i, j)
+
+        return (-1, -1)
+
+
+mat = [[1, 4], [3, 2]]
+mat = [[10, 20, 15], [21, 30, 14], [7, 16, 32]]
+print(Solution().findPeakGrid(mat))
