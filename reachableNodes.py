161fd2e771915df03d92e5a30733d16390595266
f4prime <zzz136454872@163.com>
2024-03-02 21:39:04 +0800

    add one

--- reachableNodes.py
+++ reachableNodes.py
@@ -1,9 +1,10 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from heapq import heappop, heappush
 from typing import List
 
 
-class Solution:
+# 不知道是哪个
+class Solution1:
     def reachableNodes(self, edges: List[List[int]], maxMoves: int,
                        n: int) -> int:
         link = defaultdict(lambda: {})
@@ -49,13 +50,51 @@ class Solution:
         return res
 
 
-edges = [[0, 1, 10], [0, 2, 1], [1, 2, 2]]
-maxMoves = 6
-n = 3
-edges = [[0, 1, 4], [1, 2, 6], [0, 2, 8], [1, 3, 1]]
-maxMoves = 10
-n = 4
-edges = [[1, 2, 4], [1, 4, 5], [1, 3, 1], [2, 3, 4], [3, 4, 5]]
-maxMoves = 17
-n = 5
-print(Solution().reachableNodes(edges, maxMoves, n))
+# edges = [[0, 1, 10], [0, 2, 1], [1, 2, 2]]
+# maxMoves = 6
+# n = 3
+# edges = [[0, 1, 4], [1, 2, 6], [0, 2, 8], [1, 3, 1]]
+# maxMoves = 10
+# n = 4
+# edges = [[1, 2, 4], [1, 4, 5], [1, 3, 1], [2, 3, 4], [3, 4, 5]]
+# maxMoves = 17
+# n = 5
+# print(Solution().reachableNodes(edges, maxMoves, n))
+
+
+# 2368. 受限条件下可到达节点的数目
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]],
+                       restricted: List[int]) -> int:
+        link = [[] for i in range(n)]
+        r = set(restricted)
+
+        for s, t in edges:
+            if s in r or t in r:
+                continue
+            link[s].append(t)
+            link[t].append(s)
+
+        q = deque([(-1, 0)])
+        res = 1
+
+        while len(q) > 0:
+            from_, now = q.popleft()
+
+            for nxt in link[now]:
+                if nxt == from_:
+                    continue
+                res += 1
+                q.append((now, nxt))
+
+        return res
+
+
+n = 7
+edges = [[0, 1], [1, 2], [3, 1], [4, 0], [0, 5], [5, 6]]
+restricted = [4, 5]
+n = 7
+edges = [[0, 1], [0, 2], [0, 5], [0, 4], [3, 2], [6, 5]]
+restricted = [4, 2, 1]
+
+print(Solution().reachableNodes(n, edges, restricted))
